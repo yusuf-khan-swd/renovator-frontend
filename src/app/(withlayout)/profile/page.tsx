bc70ab6bbc130e646246260f6d02da8c7b102de0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-14 21:23:32 +0600

    load user profile data

--- src/app/(withlayout)/profile/page.tsx
+++ src/app/(withlayout)/profile/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { getUserInfo } from "@/services/auth.service";
+import { useProfileQuery } from "@/redux/api/profileApi";
 
 const ProfilePage = () => {
-  const { userId } = getUserInfo() as any;
-  console.log(userId);
+  const { data, isLoading } = useProfileQuery(undefined);
+
+  console.log(data);
 
   return (
     <div>
-      <h1>Welcome back to your profile id: {userId}</h1>
+      <h1>Welcome back to your profile id: {data?.id}</h1>
     </div>
   );
 };
