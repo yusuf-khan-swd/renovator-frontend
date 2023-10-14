3be5936f0a41f41702e8ad5f9c25bd54834e08a4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 00:02:15 +0600

    add admin form

---
+++ src/components/AdminForm/AdminForm.tsx
@@ -0,0 +1,105 @@
+"use client";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { useCreateAdminMutation } from "@/redux/api/authApi";
+import { userSchema } from "@/schemas/user";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Col, Row, message } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+import { SubmitHandler } from "react-hook-form";
+import loginImage from "../../assets/login-image.png";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
+
+const AdminFormPage = () => {
+  const [createAdmin] = useCreateAdminMutation();
+
+  // console.log(isLoggedIn());
+
+  const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
+    try {
+      message.loading("Creating...");
+      await createAdmin({ ...data });
+
+      message.success("Admin created successfully!");
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={10}>
+        <Image src={loginImage} width={500} alt="login image" />
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1
+          style={{
+            margin: "15px 0px",
+          }}
+        >
+          Create an account
+        </h1>
+        <div>
+          <Form submitHandler={onSubmit} resolver={yupResolver(userSchema)}>
+            <div>
+              <FormInput
+                name="name"
+                type="text"
+                size="large"
+                label="Name"
+                required
+              />
+            </div>
+            <div
+              style={{
+                margin: "15px 0px",
+              }}
+            >
+              <FormInput
+                name="email"
+                type="text"
+                size="large"
+                label="Email"
+                required
+              />
+            </div>
+            <div>
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="Password"
+                required
+              />
+            </div>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{
+                margin: "10px 0px",
+              }}
+            >
+              Create Admin
+            </Button>
+          </Form>
+          <p style={{ margin: "8px 4px" }}>
+            Create user account? <Link href="/signup">signup</Link>
+          </p>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default AdminFormPage;
