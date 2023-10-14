0efd63ae3b3d442f6a821194eb94885849d7025e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-14 22:48:21 +0600

    add super admin page

--- src/app/(withlayout)/super_admin/page.tsx
+++ src/app/(withlayout)/super_admin/page.tsx
@@ -1,17 +1,89 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import { useProfileQuery } from "@/redux/api/profileApi";
+import { getUserInfo } from "@/services/auth.service";
+import { Button, Col, Row, message } from "antd";
+import Link from "next/link";
 
 const SuperAdminPage = () => {
+  const { data, isLoading } = useProfileQuery(undefined);
+
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (error: any) {
+      console.error(error);
+      message.error(error.message);
+    }
+  };
+
+  // @ts-ignore
+  const defaultValues = {
+    name: data?.name || "",
+    email: data?.email || "",
+    password: data?.password || "",
+    role: data?.role || "",
+  };
+
+  const { role } = getUserInfo() as any;
+  const base = role as string;
+
   return (
     <div>
       <UMBreadCrumb
         items={[
           {
-            label: "super_admin",
-            link: "/super_admin",
+            label: `${base}`,
+            link: `/${base}`,
           },
         ]}
       />
-      <h1>This page is for super admin</h1>
+
+      <div style={{ padding: "20px" }}>
+        <h3>{base.toUpperCase()} Profile</h3>
+
+        <Form submitHandler={onSubmit} defaultValues={defaultValues}>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput readOnly={true} name="name" label="Name" />
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput readOnly={true} name="email" label="Email" />
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput
+                readOnly={true}
+                type="password"
+                name="password"
+                label="Password"
+              />
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput readOnly={true} name="role" label="User Role" />
+            </Col>
+          </Row>
+        </Form>
+        <Link href={`/profile/edit/${data?.id}`}>
+          <Button
+            style={{
+              margin: "0px 5px",
+            }}
+            onClick={() => console.log(data)}
+            type="primary"
+          >
+            Edit
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
