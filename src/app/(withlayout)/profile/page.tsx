9b05366b8d0c41929b3c5d2871409ab6b4e111e0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-14 20:28:47 +0600

    get user id from local storage

--- src/app/(withlayout)/profile/page.tsx
+++ src/app/(withlayout)/profile/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import { getUserInfo } from "@/services/auth.service";
+
 const ProfilePage = () => {
+  const { userId } = getUserInfo() as any;
+  console.log(userId);
+
   return (
     <div>
-      <h1>Welcome back to your profile</h1>
+      <h1>Welcome back to your profile id: {userId}</h1>
     </div>
   );
 };
