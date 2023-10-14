9c6f1e785827c7a7ab999a086e302d2526ae5bc7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-14 16:57:37 +0600

    fix the condition

--- src/app/(withlayout)/layout.tsx
+++ src/app/(withlayout)/layout.tsx
@@ -11,13 +11,13 @@ import SideBar from "../../components/ui/Sidebar";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const userLoggedIn = isLoggedIn();
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (!userLoggedIn) {
       router.push("/login");
     }
-    setIsLoading(true);
+    setIsLoading(false);
   }, [router, isLoading, userLoggedIn]);
 
   if (isLoading) {
