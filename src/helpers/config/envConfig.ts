190209f1907f317524d9107676109a38e06eeafe
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-14 16:56:49 +0600

    conditionally render remote or local host

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,3 +1,7 @@
 export const getBaseUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3030/api/v1";
+  return (
+    (process.env.NODE_ENV === "production" &&
+      process.env.NEXT_PUBLIC_API_BASE_URL) ||
+    "http://localhost:5000/api/v1"
+  );
 };
--- src/redux/api/authApi.ts
+++ src/redux/api/authApi.ts
@@ -1,5 +1,6 @@
 import { tagTypes } from "../tag-types";
 import { baseApi } from "./baseApi";
+
 const AUTH_URL = "/auth";
 
 export const authApi = baseApi.injectEndpoints({
