a5fc6fd7dd393f5c1a5c2ed9df4e9c859c25dda8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 19:47:36 +0600

    add manage-user for admin

---
+++ src/app/(withlayout)/admin/manage-users/page.tsx
@@ -0,0 +1,163 @@
+"use client";
+import ActionBar from "@/components/ui/ActionBar";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import UMConfirmModal from "@/components/ui/ConfirmModal";
+import UMTable from "@/components/ui/UMTable";
+import { useDeleteUserMutation, useUsersQuery } from "@/redux/api/userApi";
+import { getUserInfo } from "@/services/auth.service";
+import { EditOutlined, EyeOutlined, ReloadOutlined } from "@ant-design/icons";
+import { Button, Input, message } from "antd";
+import Link from "next/link";
+import { useState } from "react";
+
+const AdminPage = () => {
+  const query: Record<string, any> = {};
+  const [deleteUser] = useDeleteUserMutation();
+
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+
+  const { data, isLoading } = useUsersQuery(undefined);
+
+  const deleteUserHandler = async (id: string) => {
+    // console.log(id);
+    try {
+      const res = await deleteUser(id);
+      if (res) {
+        message.success("User Successfully Deleted!");
+      }
+    } catch (error: any) {
+      message.error(error.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+    },
+
+    {
+      title: "Role",
+      dataIndex: "role",
+    },
+
+    {
+      title: "Action",
+      dataIndex: "id",
+      render: function (data: any) {
+        return (
+          <>
+            <Link href={`/${base}/manage-users/details/${data}`}>
+              <Button onClick={() => console.log(data)} type="primary">
+                <EyeOutlined />
+              </Button>
+            </Link>
+            <Link href={`/${base}/manage-users/edit/${data}`}>
+              <Button
+                style={{
+                  margin: "0px 5px",
+                }}
+                onClick={() => console.log(data)}
+                type="primary"
+              >
+                <EditOutlined />
+              </Button>
+            </Link>
+
+            <UMConfirmModal
+              id={data}
+              handleDelete={deleteUserHandler}
+              title="Do you want to remove this user?"
+              content={`Remove this user id: ${data}`}
+            />
+          </>
+        );
+      },
+    },
+  ];
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+    setPage(page);
+    setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
+  const resetFilters = () => {
+    setSortBy("");
+    setSortOrder("");
+    setSearchTerm("");
+  };
+
+  const { role } = getUserInfo() as any;
+  const base = role;
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: `manage-users`,
+            link: `/${base}/manage-users`,
+          },
+        ]}
+      />
+      <ActionBar title="User List">
+        <Input
+          size="large"
+          placeholder="Search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          style={{
+            width: "20%",
+          }}
+        />
+        <div>
+          {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button
+              style={{ margin: "0px 5px" }}
+              type="primary"
+              onClick={resetFilters}
+            >
+              <ReloadOutlined />
+            </Button>
+          )}
+        </div>
+      </ActionBar>
+
+      <UMTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={data}
+        pageSize={size}
+        totalPages={0}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
+    </div>
+  );
+};
+
+export default AdminPage;
