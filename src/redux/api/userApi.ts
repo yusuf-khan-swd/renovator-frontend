73adc968eae5fb3bd7316910d457c732119a6244
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 11:44:12 +0600

    add user api

---
+++ src/redux/api/userApi.ts
@@ -0,0 +1,50 @@
+import { tagTypes } from "../tag-types";
+import { baseApi } from "./baseApi";
+
+const USERS_URL = "/users";
+
+export const adminApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    users: build.query({
+      query: () => {
+        return {
+          url: USERS_URL,
+          method: "GET",
+        };
+      },
+      providesTags: [tagTypes.user],
+    }),
+
+    user: build.query({
+      query: (id: string | string[] | undefined) => ({
+        url: `${USERS_URL}/${id}`,
+        method: "GET",
+      }),
+      providesTags: [tagTypes.user],
+    }),
+
+    updateUser: build.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.id}`,
+        method: "PATCH",
+        data,
+      }),
+      invalidatesTags: [tagTypes.user],
+    }),
+
+    deleteUser: build.mutation({
+      query: (id) => ({
+        url: `${USERS_URL}/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.user],
+    }),
+  }),
+});
+
+export const {
+  useUsersQuery,
+  useUserQuery,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+} = adminApi;
