72a008b2cf695ad0eb38a38bbde5193e2d55abb1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 23:14:17 +0600

    add profile link to home page

--- src/app/(mainlayout)/layout.tsx
+++ src/app/(mainlayout)/layout.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <h1>NavBar Main layout</h1>
+      <h1>NavBar of Main layout</h1>
+      <h3>
+        <Link href="/profile">profile</Link>
+      </h3>
       {children}
     </div>
   );
