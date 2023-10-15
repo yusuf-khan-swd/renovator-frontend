cda6eadfca1e79d5e22e90d016e0ff5b2ed6fa7a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 22:43:46 +0600

    add service tag types

--- src/redux/tag-types.ts
+++ src/redux/tag-types.ts
@@ -1,7 +1,13 @@
 export enum tagTypes {
   profile = "profile",
   user = "user",
+  service = "service",
   admin = "admin",
 }
 
-export const tagTypesList = [tagTypes.profile, tagTypes.user, tagTypes.admin];
+export const tagTypesList = [
+  tagTypes.profile,
+  tagTypes.user,
+  tagTypes.service,
+  tagTypes.admin,
+];
