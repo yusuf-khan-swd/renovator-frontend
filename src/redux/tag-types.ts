c12249edcc2fec6ff7485881a2d67d5a13e1d272
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 21:45:07 +0600

    remove student api and update tag type with admin

--- src/redux/tag-types.ts
+++ src/redux/tag-types.ts
@@ -1,6 +1,7 @@
 export enum tagTypes {
   profile = "profile",
   user = "user",
+  admin = "admin",
 }
 
-export const tagTypesList = [tagTypes.profile, tagTypes.user];
+export const tagTypesList = [tagTypes.profile, tagTypes.user, tagTypes.admin];
