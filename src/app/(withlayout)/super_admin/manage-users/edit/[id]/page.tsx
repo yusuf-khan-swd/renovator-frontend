4b3cdb25fc8510c758dd6c919687289e3543cd57
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 17:02:04 +0600

    update user

--- src/app/(withlayout)/super_admin/manage-users/edit/[id]/page.tsx
+++ src/app/(withlayout)/super_admin/manage-users/edit/[id]/page.tsx
@@ -1,297 +1,98 @@
 "use client";
 
 import Form from "@/components/Forms/Form";
-import FormDatePicker from "@/components/Forms/FormDatePicker";
 import FormInput from "@/components/Forms/FormInput";
-import FormSelectField from "@/components/Forms/FormSelectField";
-import FormTextArea from "@/components/Forms/FormTextArea";
+import FormSelectField, {
+  SelectOptions,
+} from "@/components/Forms/FormSelectField";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { bloodGroupOptions, genderOptions } from "@/constants/global";
-import { useAdminQuery, useUpdateAdminMutation } from "@/redux/api/adminApi";
-import { useDepartmentsQuery } from "@/redux/api/departmentApi";
-import { IDepartment } from "@/types";
+import { roleOptions } from "@/constants/global";
+import { useUpdateUserMutation, useUserQuery } from "@/redux/api/userApi";
+import { getUserInfo } from "@/services/auth.service";
 
 import { Button, Col, Row, message } from "antd";
 
-const EditAdminPage = ({ params }: any) => {
-  const { data: adminData, isLoading: loading } = useAdminQuery(params?.id);
-  //   console.log(adminData);
-  const [updateAdmin] = useUpdateAdminMutation();
+const EditUserPage = ({ params }: any) => {
+  const id = params?.id;
+  const { data, isLoading } = useUserQuery(id);
+  const [updateUser] = useUpdateUserMutation();
 
-  const { data, isLoading } = useDepartmentsQuery({ limit: 100, page: 1 });
-  //@ts-ignore
-  const departments: IDepartment[] = data?.departments;
-
-  const departmentOptions =
-    departments &&
-    departments?.map((department) => {
-      return {
-        label: department?.title,
-        value: department?.id,
-      };
-    });
-
-  const onSubmit = async (values: any) => {
+  const onSubmit = async (data: any) => {
     try {
-      const res = await updateAdmin({ id: params?.id, body: values }).unwrap();
-      // console.log(res);
-      if (res?.id) {
-        message.success("Admin Successfully Updated!");
-      }
-    } catch (err: any) {
-      console.error(err.message);
+      message.loading("Updating.....");
+      // console.log(data);
+      await updateUser(data);
+
+      message.success("User updated successfully");
+    } catch (error: any) {
+      console.error(error);
+      message.error(error.message);
     }
   };
 
+  // @ts-ignore
   const defaultValues = {
-    name: {
-      firstName: adminData?.name?.firstName || "",
-      lastName: adminData?.name?.lastName || "",
-      middleName: adminData?.name?.middleName || "",
-    },
-    dateOfBirth: adminData?.dateOfBirth || "",
-    email: adminData?.email || "",
-    designation: adminData?.designation || "",
-    contactNo: adminData?.contactNo || "",
-    emergencyContactNo: adminData?.emergencyContactNo || "",
-    permanentAddress: adminData?.permanentAddress || "",
-    presentAddress: adminData?.presentAddress || "",
-    bloodGroup: adminData?.bloodGroup || "",
-    gender: adminData?.gender || "",
-    managementDepartment: adminData?.managementDepartment?.id || "",
+    id: data?.id,
+    name: data?.name || "",
+    email: data?.email || "",
+    role: data?.role || "",
   };
+
+  const { role } = getUserInfo() as any;
+  const base = role as string;
 
   return (
     <div>
       <CommonBreadCrumb
         items={[
           {
-            label: "super_admin",
-            link: "/super_admin",
+            label: `${base}`,
+            link: `/${base}`,
           },
           {
-            label: "admin",
-            link: "/super_admin/admin",
+            label: `manage-users`,
+            link: `/${base}/manage-users`,
+          },
+          {
+            label: "edit",
+            link: `/${base}/manage-users/edit`,
           },
         ]}
       />
-      <h1>Edit Admin {params?.id}</h1>
 
-      <div>
+      <div style={{ padding: "20px" }}>
+        <h3>Update User</h3>
+
         <Form submitHandler={onSubmit} defaultValues={defaultValues}>
-          <div
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput name="name" label="Name" />
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput name="email" label="Email" />
+            </Col>
+          </Row>
+
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormSelectField
+                name="role"
+                label="User Role"
+                options={roleOptions as SelectOptions[]}
+              />
+            </Col>
+          </Row>
+
+          <Button
+            htmlType="submit"
             style={{
-              border: "1px solid #d9d9d9",
-              borderRadius: "5px",
-              padding: "15px",
-              marginBottom: "10px",
+              margin: "0px 5px",
             }}
+            onClick={() => console.log(data)}
+            type="primary"
           >
-            <p
-              style={{
-                fontSize: "18px",
-                marginBottom: "10px",
-              }}
-            >
-              Admin Information
-            </p>
-            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormInput
-                  type="text"
-                  name="name.firstName"
-                  size="large"
-                  label="First Name"
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormInput
-                  type="text"
-                  name="name.middleName"
-                  size="large"
-                  label="Middle Name"
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormInput
-                  type="text"
-                  name="name.lastName"
-                  size="large"
-                  label="Last Name"
-                />
-              </Col>
-
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormSelectField
-                  size="large"
-                  name="gender"
-                  options={genderOptions}
-                  label="Gender"
-                  placeholder="Select"
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormSelectField
-                  size="large"
-                  name="managementDepartment"
-                  options={departmentOptions}
-                  label="Department"
-                  placeholder="Select"
-                />
-              </Col>
-            </Row>
-          </div>
-
-          {/* basic info */}
-          <div
-            style={{
-              border: "1px solid #d9d9d9",
-              borderRadius: "5px",
-              padding: "15px",
-              marginBottom: "10px",
-            }}
-          >
-            <p
-              style={{
-                fontSize: "18px",
-                marginBottom: "10px",
-              }}
-            >
-              Basic Information
-            </p>
-            <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormInput
-                  type="email"
-                  name="email"
-                  size="large"
-                  label="Email address"
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormInput
-                  type="text"
-                  name="contactNo"
-                  size="large"
-                  label="Contact No."
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormInput
-                  type="text"
-                  name="emergencyContactNo"
-                  size="large"
-                  label="Emergency Contact No."
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormDatePicker
-                  name="dateOfBirth"
-                  label="Date of birth"
-                  size="large"
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormSelectField
-                  size="large"
-                  name="bloodGroup"
-                  options={bloodGroupOptions}
-                  label="Blood group"
-                  placeholder="Select"
-                />
-              </Col>
-              <Col
-                className="gutter-row"
-                span={8}
-                style={{
-                  marginBottom: "10px",
-                }}
-              >
-                <FormInput
-                  type="text"
-                  name="designation"
-                  size="large"
-                  label="Designation"
-                />
-              </Col>
-              <Col span={12} style={{ margin: "10px 0" }}>
-                <FormTextArea
-                  name="presentAddress"
-                  label="Present address"
-                  rows={4}
-                />
-              </Col>
-
-              <Col span={12} style={{ margin: "10px 0" }}>
-                <FormTextArea
-                  name="permanentAddress"
-                  label="Permanent address"
-                  rows={4}
-                />
-              </Col>
-            </Row>
-          </div>
-          <Button htmlType="submit" type="primary">
             Update
           </Button>
         </Form>
@@ -300,4 +101,4 @@ const EditAdminPage = ({ params }: any) => {
   );
 };
 
-export default EditAdminPage;
+export default EditUserPage;
