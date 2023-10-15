3480807c9c97fb05ee2c2591218239144860b057
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-15 22:43:36 +0600

    remove api folder

--- src/app/api/file/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiResponse } from "next";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(req: NextRequest, res: NextApiResponse) {
-  return NextResponse.json({ name: "File uploaded" });
-}
