62c4b549d9fc206dabc667f267a1e5e620b49c63
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-16 22:30:42 +0600

    Add user page

---
+++ src/app/(withlayout)/user/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import { useProfileQuery } from "@/redux/api/profileApi";
+import { getUserInfo } from "@/services/auth.service";
+import { Button, Col, Row, message } from "antd";
+import Link from "next/link";
+
+const UserPage = () => {
+  const { data, isLoading } = useProfileQuery(undefined);
+
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (error: any) {
+      console.error(error);
+      message.error(error.message);
+    }
+  };
+
+  // @ts-ignore
+  const defaultValues = {
+    name: data?.name || "",
+    email: data?.email || "",
+    password: data?.password || "",
+    role: data?.role || "",
+  };
+
+  const { role } = getUserInfo() as any;
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          {
+            label: `${role}`,
+            link: `/${role}`,
+          },
+        ]}
+      />
+
+      <div style={{ padding: "20px" }}>
+        <h3>{role.toUpperCase()} Profile</h3>
+
+        <Form submitHandler={onSubmit} defaultValues={defaultValues}>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput readOnly={true} name="name" label="Name" />
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput readOnly={true} name="email" label="Email" />
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput
+                readOnly={true}
+                type="password"
+                name="password"
+                label="Password"
+              />
+            </Col>
+          </Row>
+          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+            <Col span={8} style={{ margin: "10px 0" }}>
+              <FormInput readOnly={true} name="role" label="User Role" />
+            </Col>
+          </Row>
+        </Form>
+        <Link href={`/profile/edit/${data?.id}`}>
+          <Button
+            style={{
+              margin: "0px 5px",
+            }}
+            onClick={() => console.log(data)}
+            type="primary"
+          >
+            Edit
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserPage;
