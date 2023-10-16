f0d3594542fab9a7caa373e5c0e08158f4139980
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-16 22:32:42 +0600

    Add user page

---
+++ src/app/(withlayout)/user/manage-cart/page.tsx
@@ -0,0 +1,184 @@
+"use client";
+import ActionBar from "@/components/ui/ActionBar";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import CommonTable from "@/components/ui/CommonTable";
+import ConfirmModal from "@/components/ui/ConfirmModal";
+import {
+  useBookingsQuery,
+  useDeleteBookingMutation,
+} from "@/redux/api/bookingApi";
+import { useDebounced } from "@/redux/hooks";
+import { getUserInfo } from "@/services/auth.service";
+import { EditOutlined, ReloadOutlined } from "@ant-design/icons";
+import { Button, Input, message } from "antd";
+import dayjs from "dayjs";
+import Link from "next/link";
+import { useState } from "react";
+
+const ManageBookingPage = () => {
+  const { role } = getUserInfo() as any;
+  const routeName = "manage-bookings";
+
+  const query: Record<string, any> = {};
+
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const [deleteBooking] = useDeleteBookingMutation();
+
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+  // query["searchTerm"] = searchTerm;
+
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query["searchTerm"] = debouncedTerm;
+  }
+  const { data, isLoading } = useBookingsQuery(undefined);
+  // console.log(data);
+
+  const deleteHandler = async (id: string) => {
+    message.loading("Deleting.....");
+    try {
+      //   console.log(data);
+      await deleteBooking(id);
+      message.success("Booking Delete successfully");
+    } catch (err: any) {
+      //   console.error(err.message);
+      message.error(err.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "date",
+      sorter: true,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+    {
+      title: "Price",
+      dataIndex: "service.price",
+      sorter: true,
+    },
+    {
+      title: "User Email",
+      dataIndex: "user.email",
+      sorter: true,
+    },
+    {
+      title: "CreatedAt",
+      dataIndex: "createdAt",
+      render: function (data: any) {
+        return data && dayjs(data).format("MMM D, YYYY hh:mm A");
+      },
+      sorter: true,
+    },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <>
+            <Link href={`/${role}/${routeName}/edit/${data?.id}`}>
+              <Button
+                style={{
+                  margin: "0px 5px",
+                }}
+                onClick={() => console.log(data)}
+                type="primary"
+              >
+                <EditOutlined />
+              </Button>
+            </Link>
+            <ConfirmModal
+              id={data?.id}
+              handleDelete={deleteHandler}
+              title="Do you want to delete this service?"
+              content={`Delete ${data?.title} service!`}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+    setPage(page);
+    setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
+  const resetFilters = () => {
+    setSortBy("");
+    setSortOrder("");
+    setSearchTerm("");
+  };
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          { label: role, link: `/${role}` },
+          { label: routeName, link: `/${role}/${routeName}` },
+        ]}
+      />
+
+      <ActionBar title="Booking List">
+        <Input
+          type="text"
+          size="large"
+          placeholder="Search..."
+          style={{
+            width: "20%",
+          }}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <div>
+          {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button
+              onClick={resetFilters}
+              type="primary"
+              style={{ margin: "0px 5px" }}
+            >
+              <ReloadOutlined />
+            </Button>
+          )}
+        </div>
+      </ActionBar>
+
+      <CommonTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={data}
+        pageSize={size}
+        totalPages={0}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
+    </div>
+  );
+};
+
+export default ManageBookingPage;
