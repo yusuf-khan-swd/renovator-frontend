22112e3e0d32b83c25e58987eee193934a5cab3d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-17 22:28:47 +0600

    Initialize blog, faq and about us page

---
+++ src/app/(mainlayout)/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+    </div>
+  );
+};
+
+export default AboutUsPage;
---
+++ src/app/(mainlayout)/faq/page.tsx
@@ -0,0 +1,9 @@
+const FaqPage = () => {
+  return (
+    <div>
+      <h1>Faq Page</h1>
+    </div>
+  );
+};
+
+export default FaqPage;
