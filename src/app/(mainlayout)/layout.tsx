5a188aacfa6231b112fbc8dd8d7e16e07d13f4a9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-17 21:43:29 +0600

    Create Main layout header and layout

--- src/app/(mainlayout)/layout.tsx
+++ src/app/(mainlayout)/layout.tsx
@@ -1,10 +1,13 @@
+"use client";
+
+import MainNavbar from "@/components/ui/MainNavbar";
 import Link from "next/link";
 import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <h1>NavBar of Main layout</h1>
+      <MainNavbar />
       <h3>
         <Link href="/profile">profile</Link>
       </h3>
---
+++ src/components/ui/MainNavbar.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { authKey } from "@/constants/storageKey";
+import { getUserInfo, removeUserInfo } from "@/services/auth.service";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Button, Dropdown, Layout, MenuProps, Row, Space } from "antd";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+const { Header: AntHeader } = Layout;
+
+const MainNavbar = () => {
+  const router = useRouter();
+
+  const logOut = () => {
+    removeUserInfo(authKey);
+  };
+
+  const items: MenuProps["items"] = [
+    {
+      key: "0",
+      label: (
+        <Button onClick={logOut} type="text" danger>
+          Logout
+        </Button>
+      ),
+    },
+  ];
+  const { role } = getUserInfo() as any;
+  return (
+    <AntHeader
+      style={{
+        background: "#fff",
+      }}
+    >
+      <Row
+        justify="space-between"
+        align="middle"
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Link href="/home">
+          <Button type="link" style={{ fontSize: "25px", fontWeight: "bold" }}>
+            RENOVATOR
+          </Button>
+        </Link>
+        <div style={{}}>
+          <Link href="/service">
+            <Button type="link">Service</Button>
+          </Link>
+          <Link href="/blog">
+            <Button type="link">Blog</Button>
+          </Link>
+          <Link href="/faq">
+            <Button type="link">FAQ</Button>
+          </Link>
+          <Link href="/services">
+            <Button type="link">About Us</Button>
+          </Link>
+        </div>
+        <p
+          style={{
+            margin: "0px 5px",
+          }}
+        >
+          {role}
+        </p>
+        {role ? (
+          <Dropdown menu={{ items }}>
+            <a>
+              <Space wrap size={16}>
+                <Avatar icon={<UserOutlined />} />
+              </Space>
+            </a>
+          </Dropdown>
+        ) : (
+          <div>
+            <Link href="signup">
+              <Button style={{ marginRight: "5px" }}>Signup</Button>
+            </Link>
+            <Link href="/login">
+              <Button type="primary">login</Button>
+            </Link>
+          </div>
+        )}
+      </Row>
+    </AntHeader>
+  );
+};
+
+export default MainNavbar;
