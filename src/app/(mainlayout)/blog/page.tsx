fdd12676858ce2073f88526790a4cd68a90e90b5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-18 12:30:05 +0600

    use accordion component for blog and faq

--- src/app/(mainlayout)/blog/page.tsx
+++ src/app/(mainlayout)/blog/page.tsx
@@ -1,23 +1,12 @@
 "use client";
 
+import Accordion from "@/components/ui/Accordion";
 import { useBlogsQuery } from "@/redux/api/content/blogApi";
-import { Card } from "antd";
 
 const BlogPage = () => {
   const { data, isLoading } = useBlogsQuery(undefined);
 
-  return (
-    <div>
-      <h1>Blog Page</h1>
-      {data?.map((blog: any) => (
-        <div key={blog?.id}>
-          <Card hoverable title={blog?.title} style={{ width: 240 }}>
-            <p>{blog?.description}</p>
-          </Card>
-        </div>
-      ))}
-    </div>
-  );
+  return <Accordion data={data} />;
 };
 
 export default BlogPage;
--- src/app/(mainlayout)/faq/page.tsx
+++ src/app/(mainlayout)/faq/page.tsx
@@ -1,23 +1,12 @@
 "use client";
 
+import Accordion from "@/components/ui/Accordion";
 import { useFaqsQuery } from "@/redux/api/content/faqApi";
-import { Card } from "antd";
 
 const FaqPage = () => {
   const { data, isLoading } = useFaqsQuery(undefined);
 
-  return (
-    <div>
-      <h1>Faq Page</h1>
-      {data?.map((faq: any) => (
-        <div key={faq?.id}>
-          <Card hoverable title={faq?.title} style={{ width: 240 }}>
-            <p>{faq?.description}</p>
-          </Card>
-        </div>
-      ))}
-    </div>
-  );
+  return <Accordion data={data} />;
 };
 
 export default FaqPage;
