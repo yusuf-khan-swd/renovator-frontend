005c752f7ad14eaa00700ea9fcd4b4cf3670f37e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-18 12:27:05 +0600

    Create reusable accordion component

---
+++ src/components/ui/Accordion.tsx
@@ -0,0 +1,28 @@
+import type { CollapseProps } from "antd";
+import { Collapse } from "antd";
+
+interface IAccordion {
+  data: any;
+}
+
+const Accordion = ({ data }: IAccordion) => {
+  const items: CollapseProps["items"] = data?.map((item: any) => {
+    return {
+      key: item?.id,
+      label: item?.title,
+      children: <p>{item?.description}</p>,
+    };
+  });
+
+  return (
+    <div style={{ maxWidth: "750px", margin: "0 auto" }}>
+      {data && data?.length > 0 ? (
+        <Collapse accordion items={items} />
+      ) : (
+        <p>No Data Available</p>
+      )}
+    </div>
+  );
+};
+
+export default Accordion;
