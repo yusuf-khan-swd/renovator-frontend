8680c015ce5cbed33f83e93f57b4e953d58a01d8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-18 15:38:58 +0600

    Update success and error message and use confirm modal

--- src/components/ui/ServiceCard.tsx
+++ src/components/ui/ServiceCard.tsx
@@ -1,24 +1,30 @@
 import { useCreateBookingMutation } from "@/redux/api/bookingApi";
 import { useCreateCartMutation } from "@/redux/api/cartApi";
+import { getUserInfo } from "@/services/auth.service";
 import { IService } from "@/types";
 import { Button, Card, Col, message } from "antd";
 import Link from "next/link";
+import ConfirmBookingModal from "./ConfirmBookingModal";
 
 interface IServiceProps {
   service: IService;
 }
 
 const ServiceCard = ({ service }: IServiceProps) => {
+  const { role } = getUserInfo() as any;
+
   const [createCart] = useCreateCartMutation();
   const [createBooking] = useCreateBookingMutation();
 
   const handleAddToCart = async (data: any) => {
     try {
-      console.log(data);
       message.loading("Adding to cart");
-      const result = await createCart(data);
-      console.log(result);
-      message.success("Service added to cart!");
+      const result: any = await createCart(data);
+      if (result?.data) {
+        message.success("Service added to cart!");
+      } else {
+        message.error("Service adding to cart failed!");
+      }
     } catch (error: any) {
       console.log(error);
       message.error(error.message);
@@ -27,11 +33,13 @@ const ServiceCard = ({ service }: IServiceProps) => {
 
   const handleBooking = async (data: any) => {
     try {
-      console.log(data);
-      message.loading("Adding to cart");
-      const result = await createBooking(data);
-      console.log(result);
-      message.success("Service added to cart!");
+      message.loading("Service Booking...");
+      const result: any = await createBooking(data);
+      if (result?.data) {
+        message.success("Service booked successfully!!");
+      } else {
+        message.error("Service booking failed!");
+      }
     } catch (error: any) {
       console.log(error);
       message.error(error.message);
@@ -52,12 +60,22 @@ const ServiceCard = ({ service }: IServiceProps) => {
         </div>
         <div>
           <Link href={`/service/${service.id}`}>Details</Link>
-          <Button onClick={() => handleAddToCart({ serviceId: service.id })}>
-            Add to Cart
-          </Button>
-          <Button onClick={() => handleBooking({ serviceId: service.id })}>
-            Booking
-          </Button>
+          {role && (
+            <>
+              <Button
+                onClick={() => handleAddToCart({ serviceId: service.id })}
+              >
+                Add to Cart
+              </Button>
+              <Button onClick={() => handleBooking({ serviceId: service.id })}>
+                Booking
+              </Button>
+              <ConfirmBookingModal
+                id={service.id}
+                handleBooking={handleBooking}
+              />
+            </>
+          )}
         </div>
       </Card>
     </Col>
