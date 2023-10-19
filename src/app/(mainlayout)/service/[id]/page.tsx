2db2fc3ed008ecb1020dd3248e648e1f8902532a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-19 14:28:00 +0600

    Add review and rating

--- src/app/(mainlayout)/service/[id]/page.tsx
+++ src/app/(mainlayout)/service/[id]/page.tsx
@@ -1,12 +1,40 @@
 "use client";
 
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import FormTextArea from "@/components/Forms/FormTextArea";
 import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import ServiceCard from "@/components/ui/ServiceCard";
+import { useCreateReviewMutation } from "@/redux/api/reviewApi";
 import { useServiceQuery } from "@/redux/api/serviceApi";
+import { getUserInfo } from "@/services/auth.service";
+import { Button, Col, Row, message } from "antd";
 
 const ServiceDetailsPage = ({ params }: any) => {
+  const { role } = getUserInfo() as any;
+
   const id = params?.id;
   const { data, isLoading } = useServiceQuery(id);
+
+  const [createReview] = useCreateReviewMutation();
+
+  const onSubmit = async (data: any) => {
+    try {
+      message.loading("Creating.....");
+      data.serviceId = id;
+      data.rating = parseInt(data.rating);
+      const result: any = await createReview(data);
+      // console.log(result);
+      if (result?.data) {
+        message.success("Review and Rating added successfully");
+      } else {
+        message.error("Review and rating failed to add!");
+      }
+    } catch (err: any) {
+      console.error(err.message);
+      message.error(err.message);
+    }
+  };
 
   return (
     <div>
@@ -16,6 +44,29 @@ const ServiceDetailsPage = ({ params }: any) => {
       ) : (
         <ServiceCard service={data} detailsButton={false} />
       )}
+
+      <h2>Review Ratings</h2>
+      <Form submitHandler={onSubmit}>
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Col span={8} style={{ margin: "10px 0" }}>
+            <FormInput type="number" name="rating" label="Rating" required />
+          </Col>
+        </Row>
+
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Col span={16} style={{ margin: "10px 0" }}>
+            <FormTextArea
+              name="review"
+              label="Review Description"
+              rows={5}
+              required
+            />
+          </Col>
+        </Row>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
