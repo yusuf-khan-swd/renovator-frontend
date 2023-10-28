136d4da023ab0ef9f16184c400a69a71dcff9eea
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-28 11:29:36 +0600

    Show ongoing services and upcoming services in home page

--- src/app/(mainlayout)/home/page.tsx
+++ src/app/(mainlayout)/home/page.tsx
@@ -6,28 +6,10 @@ import Feedback from "@/components/ui/Feedback";
 import Service from "@/components/ui/Service";
 import {
   useOngoingServiceQuery,
-  useServicesQuery,
   useUpcomingServiceQuery,
 } from "@/redux/api/serviceApi";
-import { useState } from "react";
 
 const HomePage = () => {
-  const query: Record<string, any> = {};
-
-  const [page, setPage] = useState<number>(1);
-  const [size, setSize] = useState<number>(3);
-  const [sortBy, setSortBy] = useState<string>("");
-  const [sortOrder, setSortOrder] = useState<string>("");
-  const [searchTerm, setSearchTerm] = useState<string>("");
-
-  query["limit"] = size;
-  query["page"] = page;
-  query["sortBy"] = sortBy;
-  query["sortOrder"] = sortOrder;
-
-  const { data, isLoading } = useServicesQuery({ ...query });
-  const services = data?.services;
-
   const { data: upcomingServices, isLoading: isUpcomingServicesLoading } =
     useUpcomingServiceQuery({ limit: 2 });
 
@@ -41,8 +23,18 @@ const HomePage = () => {
   return (
     <div>
       <Banner />
-      <h2 style={{ padding: "15px 0" }}>Trending Services</h2>
-      {isLoading ? <FullScreenLoading /> : <Service services={services} />}
+      <h2 style={{ padding: "15px 0" }}>Ongoing Services</h2>
+      {isOngoingServicesLoading ? (
+        <FullScreenLoading />
+      ) : (
+        <Service services={ongoingServices} />
+      )}
+      <h2 style={{ padding: "15px 0" }}>Upcoming Services</h2>
+      {isOngoingServicesLoading ? (
+        <FullScreenLoading />
+      ) : (
+        <Service services={upcomingServices} />
+      )}
       <Feedback />
     </div>
   );
