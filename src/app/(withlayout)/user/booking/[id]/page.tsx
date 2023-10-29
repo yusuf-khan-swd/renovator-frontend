94b21479a8288cd44824ff2b877d8e30c6fddc95
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-29 12:15:58 +0600

    Remove form for to show service data

--- src/app/(withlayout)/user/booking/[id]/page.tsx
+++ src/app/(withlayout)/user/booking/[id]/page.tsx
@@ -1,24 +1,17 @@
 "use client";
 
-import CategoryField from "@/components/Forms/CategoryField";
 import Form from "@/components/Forms/Form";
 import FormDatePicker from "@/components/Forms/FormDatePicker";
-import FormInput from "@/components/Forms/FormInput";
-import FormSelectField, {
-  SelectOptions,
-} from "@/components/Forms/FormSelectField";
-import FormTextArea from "@/components/Forms/FormTextArea";
-import { serviceStatusOptions } from "@/constants/global";
 import {
   useServiceQuery,
   useUpdateServiceMutation,
 } from "@/redux/api/serviceApi";
-import { serviceSchema } from "@/schemas/service";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Col, Row, message } from "antd";
+import { getUserInfo } from "@/services/auth.service";
+import { Button, Card, Col, Row, message } from "antd";
 
 const BookingPage = ({ params }: any) => {
   const id = params?.id;
+  const { userId } = getUserInfo() as any;
   const { data: service, isLoading } = useServiceQuery(id);
 
   const [updateService] = useUpdateServiceMutation();
@@ -26,6 +19,9 @@ const BookingPage = ({ params }: any) => {
   const onSubmit = async (data: any) => {
     try {
       message.loading("Creating.....");
+      data.serviceId = id;
+      data.userId = userId;
+      data.date = new Date();
       console.log(data);
       // const result:any =  await updateService(data);
       // console.log(result)
@@ -41,62 +37,30 @@ const BookingPage = ({ params }: any) => {
   };
 
   const defaultValues = {
-    id: service?.id,
-    title: service?.title || "",
-    description: service?.description || "",
-    price: service?.price || "",
-    status: service?.status || "",
-    categoryId: service?.categoryId || "",
-    location: service?.location || "",
+    date: service?.date,
   };
 
   return (
     <div>
-      <Form
-        submitHandler={onSubmit}
-        resolver={yupResolver(serviceSchema)}
-        defaultValues={defaultValues}
-      >
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormInput name="title" label="Title" required />
-          </Col>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormInput name="price" label="Price" required />
-          </Col>
-        </Row>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <CategoryField name="categoryId" label="Category" />
-          </Col>
-        </Row>
+      <Card title={service?.title}>
+        <div style={{ paddingBottom: "15px", fontSize: "16px" }}>
+          <p>Category: {service?.category?.title}</p>
+          <p>Price: ${service?.price}</p>
+          <p>
+            Status: <span style={{ color: "green" }}>{service?.status}</span>
+          </p>
+          <p>Location: {service?.location}</p>
+          <p>Description: {service?.description}</p>
+        </div>
+      </Card>
+      <Form submitHandler={onSubmit} defaultValues={defaultValues}>
         <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
           <Col span={8} style={{ margin: "10px 0" }}>
             <FormDatePicker name="date" label="Select Booking Date" />
           </Col>
         </Row>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormInput name="location" label="Location" required />
-          </Col>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormSelectField
-              name="status"
-              label="Status"
-              options={serviceStatusOptions as SelectOptions[]}
-            />
-          </Col>
-        </Row>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormTextArea
-              name="description"
-              label="Description"
-              rows={5}
-              required
-            />
-          </Col>
-        </Row>
+        <input type="date" />
+
         <Button type="primary" htmlType="submit">
           Book Service
         </Button>
