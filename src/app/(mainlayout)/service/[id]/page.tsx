9250927b014ca8490170204a00166581f56a5652
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-12 12:35:14 +0600

    Separate review and rating form and call ReviewAndRating form component

--- src/app/(mainlayout)/service/[id]/page.tsx
+++ src/app/(mainlayout)/service/[id]/page.tsx
@@ -1,46 +1,17 @@
 "use client";
 
-import Form from "@/components/Forms/Form";
-import FormSelectField, {
-  SelectOptions,
-} from "@/components/Forms/FormSelectField";
-import FormTextArea from "@/components/Forms/FormTextArea";
+import ReviewAndRatingForm from "@/components/Forms/ReviewAndRatingForm";
 import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import Reviews from "@/components/ui/Reviews";
 import ServiceCard from "@/components/ui/ServiceCard";
-import { ratingOptions } from "@/constants/global";
-import { useCreateReviewMutation } from "@/redux/api/reviewApi";
 import { useServiceQuery } from "@/redux/api/serviceApi";
-import { reviewAndRatingSchema } from "@/schemas/reviewAndRating";
 import { getUserInfo } from "@/services/auth.service";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Col, Row, message } from "antd";
 
 const ServiceDetailsPage = ({ params }: any) => {
   const { role } = getUserInfo() as any;
 
   const id = params?.id;
   const { data: service, isLoading: isServiceLoading } = useServiceQuery(id);
-
-  const [createReview] = useCreateReviewMutation();
-
-  const onSubmit = async (data: any) => {
-    try {
-      message.loading("Creating.....");
-      data.serviceId = id;
-      data.rating = parseInt(data.rating);
-      const result: any = await createReview(data);
-      // console.log(result);
-      if (result?.data) {
-        message.success("Review and Rating added successfully");
-      } else {
-        message.error("Review and rating failed to add!");
-      }
-    } catch (err: any) {
-      console.error(err.message);
-      message.error(err.message);
-    }
-  };
 
   return (
     <div>
@@ -53,37 +24,7 @@ const ServiceDetailsPage = ({ params }: any) => {
 
       <div style={{ margin: "25px 0" }}>
         <h2 style={{ textAlign: "center" }}>Review Ratings</h2>
-        {role && (
-          <Form
-            submitHandler={onSubmit}
-            resolver={yupResolver(reviewAndRatingSchema)}
-          >
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={8} style={{ margin: "10px 0" }}>
-                <FormSelectField
-                  name="rating"
-                  label="Rating"
-                  options={ratingOptions as SelectOptions[]}
-                  required
-                />
-              </Col>
-            </Row>
-
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={16} style={{ margin: "10px 0" }}>
-                <FormTextArea
-                  name="review"
-                  label="Review Description"
-                  rows={5}
-                  required
-                />
-              </Col>
-            </Row>
-            <Button type="primary" htmlType="submit">
-              Submit a Review
-            </Button>
-          </Form>
-        )}
+        {role && <ReviewAndRatingForm serviceId={id} />}
         <div>
           <Reviews serviceId={id} />
         </div>
