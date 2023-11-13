4cff3add2625d9929b93a0bb8329f1d4a20cb2e8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-13 10:55:40 +0600

    Update text

--- src/components/ui/ReviewCard.tsx
+++ src/components/ui/ReviewCard.tsx
@@ -5,8 +5,8 @@ const ReviewCard = ({ review }: { review: any }) => {
     <Col span={24} style={{ margin: "10px 0" }}>
       <Card>
         <p>User: {review?.user?.email}</p>
-        <p>User Rating: {review?.rating}</p>
-        <p>User Review: {review?.review}</p>
+        <p>Rating: {review?.rating}</p>
+        <p>Review: {review?.review}</p>
       </Card>
     </Col>
   );
