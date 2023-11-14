c2fbbae4de1724fbe070f23abf7be0403b65e7ee
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-14 13:28:20 +0600

    If page is details page then not render link card

--- src/components/ui/ServiceCard.tsx
+++ src/components/ui/ServiceCard.tsx
@@ -53,7 +53,7 @@ const ServiceCard = ({ service, detailsPage = false }: IServiceProps) => {
         hoverable
         title={<p style={{ fontSize: "20px" }}>{service?.title}</p>}
       >
-        <Link style={{ color: "inherit" }} href={`/service/${service?.id}`}>
+        {detailsPage ? (
           <div
             style={{
               paddingBottom: "15px",
@@ -87,7 +87,44 @@ const ServiceCard = ({ service, detailsPage = false }: IServiceProps) => {
               )}
             </p>
           </div>
-        </Link>
+        ) : (
+          <Link style={{ color: "inherit" }} href={`/service/${service?.id}`}>
+            <div
+              style={{
+                paddingBottom: "15px",
+                display: "grid",
+                gap: "2px",
+              }}
+            >
+              <p>Category: {service?.category?.title}</p>
+              <p>
+                Price:{" "}
+                <span style={{ fontWeight: "bold" }}>${service?.price}</span>
+              </p>
+              <p>
+                Status:{" "}
+                <span style={{ color: "green" }}>
+                  {service?.status.charAt(0).toUpperCase() +
+                    service?.status.slice(1)}
+                </span>
+              </p>
+              <p>Location: {service?.location}</p>
+              <p style={{ padding: "5px 0" }}>
+                <span style={{ fontWeight: "bold" }}>Description:</span>{" "}
+                {detailsPage ? (
+                  <span>{service?.description}</span>
+                ) : (
+                  <span>
+                    {service?.description.length <= 150
+                      ? service?.description
+                      : service?.description.slice(0, 150) + "..."}
+                  </span>
+                )}
+              </p>
+            </div>
+          </Link>
+        )}
+
         <Row justify="space-between">
           {!detailsPage ? (
             <Link href={`/service/${service?.id}`}>
