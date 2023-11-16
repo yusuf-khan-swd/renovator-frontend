61752426842833d1f7460d49b393d7ab15be2bf8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-16 10:40:12 +0600

    update feedback schema

--- src/schemas/feedback.ts
+++ src/schemas/feedback.ts
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const feedbackSchema = yup.object().shape({
   name: yup.string().required("Your name is required"),
-  email: yup.string().required("your email is required"),
+  email: yup.string().required("Your email is required"),
   rating: yup.string().required("Rating is required"),
   review: yup.string().required("Review is required"),
 });
