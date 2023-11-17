01c76ff6baffc1b68d727c9acfe9a85848115953
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-17 12:48:04 +0600

    Show loading in user profile

--- src/app/(withlayout)/profile/page.tsx
+++ src/app/(withlayout)/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
+import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
 import { useProfileQuery } from "@/redux/api/profileApi";
 import { getUserInfo } from "@/services/auth.service";
@@ -47,45 +48,50 @@ const ProfilePage = () => {
 
       <div style={{ padding: "20px" }}>
         <h3>{role.charAt(0).toUpperCase() + role.slice(1)} Profile</h3>
-
-        <Form submitHandler={onSubmit} defaultValues={defaultValues}>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput readOnly={true} name="name" label="Name" />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput readOnly={true} name="email" label="Email" />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput
-                readOnly={true}
-                type="password"
-                name="password"
-                label="Password"
-              />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput readOnly={true} name="role" label="User Role" />
-            </Col>
-          </Row>
-        </Form>
-        <Link href={`/profile/edit/${data?.id}`}>
-          <Button
-            style={{
-              margin: "0px 5px",
-            }}
-            onClick={() => console.log(data)}
-            type="primary"
-          >
-            Edit
-          </Button>
-        </Link>
+        {isLoading ? (
+          <FullScreenLoading />
+        ) : (
+          <div>
+            <Form submitHandler={onSubmit} defaultValues={defaultValues}>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col span={8} style={{ margin: "10px 0" }}>
+                  <FormInput readOnly={true} name="name" label="Name" />
+                </Col>
+              </Row>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col span={8} style={{ margin: "10px 0" }}>
+                  <FormInput readOnly={true} name="email" label="Email" />
+                </Col>
+              </Row>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col span={8} style={{ margin: "10px 0" }}>
+                  <FormInput
+                    readOnly={true}
+                    type="password"
+                    name="password"
+                    label="Password"
+                  />
+                </Col>
+              </Row>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col span={8} style={{ margin: "10px 0" }}>
+                  <FormInput readOnly={true} name="role" label="User Role" />
+                </Col>
+              </Row>
+            </Form>
+            <Link href={`/profile/edit/${data?.id}`}>
+              <Button
+                style={{
+                  margin: "0px 5px",
+                }}
+                onClick={() => console.log(data)}
+                type="primary"
+              >
+                Edit
+              </Button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
