78ee713c0a1fdfd6a8dcee3bd7553ff71ac67089
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-24 13:30:19 +0600

    Create ReviewAndRatingHeading component for unified style of heading

---
+++ src/components/ReviewAndRating/ReviewAndRatingHeading.tsx
@@ -0,0 +1,9 @@
+const ReviewAndRatingHeading = ({ heading }: { heading: string }) => {
+  return (
+    <h2 style={{ fontSize: "28px", fontWeight: "bold", margin: "8px 0" }}>
+      {heading}
+    </h2>
+  );
+};
+
+export default ReviewAndRatingHeading;
