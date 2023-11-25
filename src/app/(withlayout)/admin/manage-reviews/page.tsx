b99e0549186ebb2fb44e0c5017517b862f96bda9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-25 10:04:28 +0600

    Show reviews in table

---
+++ src/app/(withlayout)/admin/manage-reviews/page.tsx
@@ -0,0 +1,211 @@
+"use client";
+import ActionBar from "@/components/ui/ActionBar";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import CommonTable from "@/components/ui/CommonTable";
+import ConfirmModal from "@/components/ui/ConfirmModal";
+import {
+  useAdminReviewsQuery,
+  useDeleteReviewMutation,
+} from "@/redux/api/reviewApi";
+import { useDebounced } from "@/redux/hooks";
+import { getUserInfo } from "@/services/auth.service";
+import { IService } from "@/types";
+import { EditOutlined, EyeOutlined, ReloadOutlined } from "@ant-design/icons";
+import { Button, Input, message } from "antd";
+import dayjs from "dayjs";
+import Link from "next/link";
+import { useState } from "react";
+
+const ManageReviewPage = () => {
+  const { role, userId } = getUserInfo() as any;
+  const routeName = "manage-reviews";
+
+  const query: Record<string, any> = {};
+
+  const [page, setPage] = useState<number>(1);
+  const [size, setSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>("");
+  const [sortOrder, setSortOrder] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const [deleteReview] = useDeleteReviewMutation();
+
+  query["limit"] = size;
+  query["page"] = page;
+  query["sortBy"] = sortBy;
+  query["sortOrder"] = sortOrder;
+
+  const debouncedTerm = useDebounced({
+    query: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query["searchTerm"] = debouncedTerm;
+  }
+  const { data, isLoading } = useAdminReviewsQuery(undefined);
+
+  const deleteHandler = async (id: string) => {
+    message.loading("Deleting.....");
+    try {
+      await deleteReview(id);
+      message.success("Review Delete successfully");
+    } catch (err: any) {
+      console.error(err);
+      message.error(err.message);
+    }
+  };
+
+  const columns = [
+    {
+      title: "Service Name",
+      dataIndex: "service",
+      render: function (service: IService) {
+        return service?.title.length <= 25
+          ? service?.title
+          : service?.title.slice(0, 25) + "...";
+      },
+    },
+    {
+      title: "Rating",
+      dataIndex: "rating",
+    },
+    {
+      title: "Review",
+      dataIndex: "review",
+      render: function (data: string) {
+        return data.length <= 25 ? data : data.slice(0, 25) + "...";
+      },
+    },
+    {
+      title: "Service Status",
+      dataIndex: "service",
+      render: function (service: IService) {
+        return service?.status;
+      },
+    },
+    {
+      title: "Service Price",
+      dataIndex: "service",
+      render: function (service: IService) {
+        return "$" + service?.price;
+      },
+      sorter: true,
+    },
+    {
+      title: "CreatedAt",
+      dataIndex: "createdAt",
+      render: function (data: any) {
+        return data && dayjs(data).format("MMM D, YYYY hh:mm A");
+      },
+      sorter: true,
+    },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <>
+            <Link href={`/${role}/manage-reviews/view/${data?.id}`}>
+              <Button
+                style={{
+                  margin: "0px 5px",
+                }}
+                onClick={() => console.log(data)}
+                type="primary"
+              >
+                <EyeOutlined />
+              </Button>
+            </Link>
+
+            <Link href={`/${role}/manage-reviews/edit/${data?.id}`}>
+              <Button
+                style={{
+                  margin: "0px 5px",
+                }}
+                onClick={() => console.log(data)}
+                type="primary"
+              >
+                <EditOutlined />
+              </Button>
+            </Link>
+
+            <ConfirmModal
+              id={data?.id}
+              handler={deleteHandler}
+              title="Do you want to delete this item from review?"
+              content={`Delete this item from review!`}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+    setPage(page);
+    setSize(pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+    setSortBy(field as string);
+    setSortOrder(order === "ascend" ? "asc" : "desc");
+  };
+
+  const resetFilters = () => {
+    setSortBy("");
+    setSortOrder("");
+    setSearchTerm("");
+  };
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          { label: role, link: `/${role}` },
+          { label: routeName, link: `/${role}/${routeName}` },
+        ]}
+      />
+
+      <ActionBar title="Review List">
+        <Input
+          type="text"
+          size="large"
+          placeholder="Search..."
+          style={{
+            width: "20%",
+          }}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <div>
+          {(!!sortBy || !!sortOrder || !!searchTerm) && (
+            <Button
+              onClick={resetFilters}
+              type="primary"
+              style={{ margin: "0px 5px" }}
+            >
+              <ReloadOutlined />
+            </Button>
+          )}
+        </div>
+      </ActionBar>
+
+      <CommonTable
+        loading={isLoading}
+        columns={columns}
+        dataSource={data}
+        pageSize={size}
+        totalPages={0}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
+    </div>
+  );
+};
+
+export default ManageReviewPage;
