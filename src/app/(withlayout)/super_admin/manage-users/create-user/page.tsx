9d875d46a25a1b2ec69c812fd31334b2de7f98dc
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-02 12:09:20 +0600

    Add create-user page

---
+++ src/app/(withlayout)/super_admin/manage-users/create-user/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import SignupPage from "@/components/Signup/Signup";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import { getUserInfo } from "@/services/auth.service";
+
+const page = () => {
+  const { role } = getUserInfo() as any;
+  const routeName = "manage-users";
+  const endName = "create";
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          {
+            label: `${role}`,
+            link: `/${role}`,
+          },
+          {
+            label: routeName,
+            link: `/${role}/${routeName}`,
+          },
+          {
+            label: endName,
+            link: `/${role}/${routeName}/${endName}`,
+          },
+        ]}
+      />
+      <div>
+        <SignupPage />
+      </div>
+    </div>
+  );
+};
+
+export default page;
