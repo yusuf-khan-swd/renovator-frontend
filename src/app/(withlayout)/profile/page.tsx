7f229fe0d0c1159666060f80d5b88804a72723d5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-03 12:47:21 +0600

    Remove unused code from profile page

--- src/app/(withlayout)/profile/page.tsx
+++ src/app/(withlayout)/profile/page.tsx
@@ -2,31 +2,9 @@
 
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { useProfileQuery } from "@/redux/api/profileApi";
 import { getUserInfo } from "@/services/auth.service";
-import { message } from "antd";
-
-// todo:: create a component for profile page that going to be use in all user home page
 
 const ProfilePage = () => {
-  const { data, isLoading } = useProfileQuery(undefined);
-
-  const onSubmit = async (data: any) => {
-    try {
-      console.log(data);
-    } catch (error: any) {
-      console.error(error);
-      message.error(error.message);
-    }
-  };
-
-  const defaultValues = {
-    name: data?.name || "",
-    email: data?.email || "",
-    password: data?.password || "",
-    role: data?.role || "",
-  };
-
   const { role }: { role: string } = getUserInfo() as any;
 
   return (
