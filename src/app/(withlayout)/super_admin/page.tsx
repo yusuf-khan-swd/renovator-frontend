b5eed35d3662c91e4a30609ac8e767e97dc253a4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-03 12:46:53 +0600

    Remove unused code

--- src/app/(withlayout)/super_admin/page.tsx
+++ src/app/(withlayout)/super_admin/page.tsx
@@ -2,32 +2,9 @@
 
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { useProfileQuery } from "@/redux/api/profileApi";
 import { getUserInfo } from "@/services/auth.service";
-import { message } from "antd";
-
-// todo:: create a component for profile page that going to be use in all user home page
 
 const SuperAdminPage = () => {
-  const { data, isLoading } = useProfileQuery(undefined);
-
-  const onSubmit = async (data: any) => {
-    try {
-      console.log(data);
-    } catch (error: any) {
-      console.error(error);
-      message.error(error.message);
-    }
-  };
-
-  // @ts-ignore
-  const defaultValues = {
-    name: data?.name || "",
-    email: data?.email || "",
-    password: data?.password || "",
-    role: data?.role || "",
-  };
-
   const { role } = getUserInfo() as any;
 
   return (
