4026877cd27e4c9b1a1bd9718c9bc99d806fa347
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-03 12:46:13 +0600

    Remove user info Form

--- src/app/(withlayout)/super_admin/page.tsx
+++ src/app/(withlayout)/super_admin/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import Form from "@/components/Forms/Form";
-import FormInput from "@/components/Forms/FormInput";
-import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
 import { useProfileQuery } from "@/redux/api/profileApi";
 import { getUserInfo } from "@/services/auth.service";
-import { Button, Col, Row, message } from "antd";
-import Link from "next/link";
+import { message } from "antd";
 
 // todo:: create a component for profile page that going to be use in all user home page
 
@@ -46,53 +42,6 @@ const SuperAdminPage = () => {
       />
 
       <UserProfile readonly={true} />
-
-      {isLoading ? (
-        <FullScreenLoading />
-      ) : (
-        <div style={{ padding: "20px" }}>
-          <h3>{role} Profile</h3>
-
-          <Form submitHandler={onSubmit} defaultValues={defaultValues}>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={8} style={{ margin: "10px 0" }}>
-                <FormInput readOnly={true} name="name" label="Name" />
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={8} style={{ margin: "10px 0" }}>
-                <FormInput readOnly={true} name="email" label="Email" />
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={8} style={{ margin: "10px 0" }}>
-                <FormInput
-                  readOnly={true}
-                  type="password"
-                  name="password"
-                  label="Password"
-                />
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={8} style={{ margin: "10px 0" }}>
-                <FormInput readOnly={true} name="role" label="User Role" />
-              </Col>
-            </Row>
-          </Form>
-          <Link href={`/profile/edit/${data?.id}`}>
-            <Button
-              style={{
-                margin: "0px 5px",
-              }}
-              onClick={() => console.log(data)}
-              type="primary"
-            >
-              Edit
-            </Button>
-          </Link>
-        </div>
-      )}
     </div>
   );
 };
