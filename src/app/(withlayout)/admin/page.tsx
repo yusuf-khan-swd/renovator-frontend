396dce456895503a2888fa9efdd90d58ddbeb838
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-04 12:09:50 +0600

    Remove admin profile form

--- src/app/(withlayout)/admin/page.tsx
+++ src/app/(withlayout)/admin/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import Form from "@/components/Forms/Form";
-import FormInput from "@/components/Forms/FormInput";
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
 import { useProfileQuery } from "@/redux/api/profileApi";
 import { getUserInfo } from "@/services/auth.service";
-import { Button, Col, Row, message } from "antd";
-import Link from "next/link";
+import { message } from "antd";
 
 const AdminPage = () => {
   const { data, isLoading } = useProfileQuery(undefined);
@@ -41,50 +38,7 @@ const AdminPage = () => {
           },
         ]}
       />
-
       <UserProfile />
-      <div style={{ padding: "20px" }}>
-        <h3>{role} Profile</h3>
-
-        <Form submitHandler={onSubmit} defaultValues={defaultValues}>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput readOnly={true} name="name" label="Name" />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput readOnly={true} name="email" label="Email" />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput
-                readOnly={true}
-                type="password"
-                name="password"
-                label="Password"
-              />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput readOnly={true} name="role" label="User Role" />
-            </Col>
-          </Row>
-        </Form>
-        <Link href={`/profile/edit/${data?.id}`}>
-          <Button
-            style={{
-              margin: "0px 5px",
-            }}
-            onClick={() => console.log(data)}
-            type="primary"
-          >
-            Edit
-          </Button>
-        </Link>
-      </div>
     </div>
   );
 };
