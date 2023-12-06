a9f5887f41e70926d602052c3f7ce8ab8171701a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-06 11:44:52 +0600

    Update user password schema

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -3,5 +3,10 @@ import * as yup from "yup";
 export const userSchema = yup.object().shape({
   name: yup.string().required("Name is required"),
   email: yup.string().required("Email is required"),
-  password: yup.string().min(6).max(32).required(),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(6)
+    .max(32)
+    .required(),
 });
