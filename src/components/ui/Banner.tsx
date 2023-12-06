60319b88439fb9b6aabe0dc055d0460d148dc037
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-06 12:19:25 +0600

    Add Row Col and border to Visualize section

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -1,5 +1,5 @@
 import bannerImage from "@/assets/banner.png";
-import { Button } from "antd";
+import { Button, Col, Row } from "antd";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,28 +7,40 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div style={{ display: "flex" }}>
-      <Image src={bannerImage} alt="banner Image" />
-      <div
+    <Row
+      style={{ border: "1px solid blue" }}
+      gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}
+    >
+      <Col style={{ border: "1px solid orange" }}>
+        <Image src={bannerImage} alt="banner Image" style={{ width: "100%" }} />
+      </Col>
+      <Col
         style={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
+          border: "1px solid orange",
         }}
       >
-        <h2>Welcome to</h2>
-        <h1 style={{ fontSize: "80px", fontWeight: "bold" }}>Renovator</h1>
-        <p style={{ padding: "15px 0" }}>
-          A modern theme geared towards all repairmen, contractors and
-          renovation companies.
-        </p>
-        <Link href="/service">
-          <Button size="large" type="primary">
-            Get Started
-          </Button>
-        </Link>
-      </div>
-    </div>
+        <div
+          style={{
+            border: "1px solid orange",
+          }}
+        >
+          <h2>Welcome to</h2>
+          <h1 style={{ fontSize: "80px", fontWeight: "bold" }}>Renovator</h1>
+          <p style={{ padding: "15px 0" }}>
+            A modern theme geared towards all repairmen, contractors and
+            renovation companies.
+          </p>
+          <Link href="/service">
+            <Button size="large" type="primary">
+              Get Started
+            </Button>
+          </Link>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
