7b297e5553fbfc4eea28f0bee44b9a79d19585ed
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-07 14:18:06 +0600

    Add max-width 1280px to main layout and center align the page

--- src/app/(mainlayout)/layout.tsx
+++ src/app/(mainlayout)/layout.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
+    <div style={{ maxWidth: "1280px", margin: "0 auto" }}>
       <MainNavbar />
       <div style={{ padding: "20px 50px", minHeight: "100vh" }}>{children}</div>
       <CallOut />
