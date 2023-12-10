a2b0d413f6a6a73914cdd275ba0994a24ae73a3e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-10 13:11:04 +0600

    Use card in edit page

--- src/app/(withlayout)/admin/manage-services/edit/[id]/page.tsx
+++ src/app/(withlayout)/admin/manage-services/edit/[id]/page.tsx
@@ -16,7 +16,7 @@ import {
 import { serviceSchema } from "@/schemas/service";
 import { getUserInfo } from "@/services/auth.service";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Col, Row, message } from "antd";
+import { Button, Card, Col, Row, message } from "antd";
 
 const EditServicePage = ({ params }: any) => {
   const id = params?.id;
@@ -59,51 +59,53 @@ const EditServicePage = ({ params }: any) => {
         ]}
       />
       <div style={{ margin: "24px 5px" }}>
-        <h1>Update service</h1>
-        <Form
-          submitHandler={onSubmit}
-          resolver={yupResolver(serviceSchema)}
-          defaultValues={defaultValues}
-        >
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput name="title" label="Title" required />
-            </Col>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput name="price" label="Price" required />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <CategoryField name="categoryId" label="Category" />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormInput name="location" label="Location" required />
-            </Col>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormSelectField
-                name="status"
-                label="Status"
-                options={serviceStatusOptions as SelectOptions[]}
-              />
-            </Col>
-          </Row>
-          <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-            <Col span={8} style={{ margin: "10px 0" }}>
-              <FormTextArea
-                name="description"
-                label="Description"
-                rows={5}
-                required
-              />
-            </Col>
-          </Row>
-          <Button type="primary" htmlType="submit">
-            Update
-          </Button>
-        </Form>
+        <Card>
+          <h1>Update service</h1>
+          <Form
+            submitHandler={onSubmit}
+            resolver={yupResolver(serviceSchema)}
+            defaultValues={defaultValues}
+          >
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormInput name="title" label="Title" required />
+              </Col>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormInput name="price" label="Price" required />
+              </Col>
+            </Row>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <CategoryField name="categoryId" label="Category" />
+              </Col>
+            </Row>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormInput name="location" label="Location" required />
+              </Col>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormSelectField
+                  name="status"
+                  label="Status"
+                  options={serviceStatusOptions as SelectOptions[]}
+                />
+              </Col>
+            </Row>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormTextArea
+                  name="description"
+                  label="Description"
+                  rows={5}
+                  required
+                />
+              </Col>
+            </Row>
+            <Button type="primary" htmlType="submit">
+              Update
+            </Button>
+          </Form>
+        </Card>
       </div>
     </div>
   );
