bc5742302051d3f343a09a002344ccd5270b112e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-14 13:06:30 +0600

    Capitalize location

--- src/app/(withlayout)/admin/manage-services/page.tsx
+++ src/app/(withlayout)/admin/manage-services/page.tsx
@@ -101,6 +101,9 @@ const ManageServicePage = () => {
     {
       title: "Location",
       dataIndex: "location",
+      render: function (data: string) {
+        return data.charAt(0).toUpperCase() + data.slice(1).toLowerCase();
+      },
       sorter: true,
     },
     ,
