805ef65c0a8abe3a6e83bb752973913e94145703
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-15 12:33:15 +0600

    Remove all lowercase in location

--- src/app/(withlayout)/admin/manage-services/page.tsx
+++ src/app/(withlayout)/admin/manage-services/page.tsx
@@ -103,7 +103,7 @@ const ManageServicePage = () => {
       title: "Location",
       dataIndex: "location",
       render: function (data: string) {
-        return data.charAt(0).toUpperCase() + data.slice(1).toLowerCase();
+        return data.charAt(0).toUpperCase() + data.slice(1);
       },
       sorter: true,
     },
