e1f5736e37b753153d4eead295eb59bf17cc7570
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-16 13:08:50 +0600

    call MainSidebarItems

--- src/components/ui/MainNavbar.tsx
+++ src/components/ui/MainNavbar.tsx
@@ -1,42 +1,11 @@
 "use client";
 
-import { authKey } from "@/constants/storageKey";
-import { getUserInfo, removeUserInfo } from "@/services/auth.service";
-import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Button, Dropdown, Layout, MenuProps, Row, Space } from "antd";
+import { Button, Layout, Row } from "antd";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import MainSidebarItems from "./MainSidebarItems";
 const { Header: AntHeader } = Layout;
 
 const MainNavbar = () => {
-  const router = useRouter();
-
-  const logOut = () => {
-    removeUserInfo(authKey);
-    router.refresh();
-  };
-
-  const items: MenuProps["items"] = [
-    {
-      key: "0",
-      label: (
-        <Link href="/profile">
-          <Button type="link">Profile</Button>
-        </Link>
-      ),
-    },
-    {
-      key: "1",
-      label: (
-        <Button onClick={logOut} type="text" danger>
-          Logout
-        </Button>
-      ),
-    },
-  ];
-
-  const { role } = getUserInfo() as any;
-
   return (
     <AntHeader
       style={{
@@ -74,22 +43,7 @@ const MainNavbar = () => {
           </Link>
         </div>
 
-        {role ? (
-          <Dropdown menu={{ items }}>
-            <Space wrap size={16}>
-              <Avatar icon={<UserOutlined />} />
-            </Space>
-          </Dropdown>
-        ) : (
-          <div>
-            <Link href="signup">
-              <Button style={{ marginRight: "5px" }}>Signup</Button>
-            </Link>
-            <Link href="/login">
-              <Button type="primary">login</Button>
-            </Link>
-          </div>
-        )}
+        <MainSidebarItems />
       </Row>
     </AntHeader>
   );
