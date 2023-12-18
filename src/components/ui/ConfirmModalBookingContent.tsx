1cc8a9d39e79cab970ae7288a599ebf7d799b699
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-18 12:44:54 +0600

    Create ConfirmModalBookingContent component to show booking info

---
+++ src/components/ui/ConfirmModalBookingContent.tsx
@@ -0,0 +1,32 @@
+import dayjs from "dayjs";
+
+const ConfirmModalBookingContent = ({ data }: { data: any }) => {
+  return (
+    <div>
+      <p>
+        Service Name:{" "}
+        <span style={{ fontWeight: "bold" }}>{data?.service?.title}</span>
+      </p>
+      <p>
+        Service Price:{" "}
+        <span style={{ fontWeight: "bold" }}>{data?.service?.price}</span>
+      </p>
+      <p>
+        Booking Date:{" "}
+        <span style={{ fontWeight: "bold" }}>
+          {dayjs(data?.date).format("MMM D, YYYY")}
+        </span>
+      </p>
+      <p>
+        Current Status:{" "}
+        <span style={{ fontWeight: "bold" }}>{data?.status}</span>
+      </p>
+      <p>
+        User Email:{" "}
+        <span style={{ fontWeight: "bold" }}>{data?.user?.email}</span>
+      </p>
+    </div>
+  );
+};
+
+export default ConfirmModalBookingContent;
