ef9b0087a0c24c90b2bc26c5ab0c45b82fafdd52
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-22 12:30:47 +0600

    Remove service status, price from ModalReview

--- src/components/ui/ConfirmModalReviewContent.tsx
+++ src/components/ui/ConfirmModalReviewContent.tsx
@@ -1,18 +1,8 @@
-import { ENUM_SERVICE_STATUS } from "@/constants/serviceStatus";
-
 interface IReviewProps {
   data: any;
 }
 
 const ConfirmModalReviewContent = ({ data }: IReviewProps) => {
-  const statusColor = `${
-    data?.service?.status === ENUM_SERVICE_STATUS.AVAILABLE
-      ? "green"
-      : data?.service?.status === ENUM_SERVICE_STATUS.UPCOMING
-      ? "blue"
-      : "red"
-  }`;
-
   return (
     <div style={{ display: "grid", gap: "2px" }}>
       <p>
@@ -22,19 +12,8 @@ const ConfirmModalReviewContent = ({ data }: IReviewProps) => {
         Email: <span style={{ fontWeight: "bold" }}>{data?.user?.email}</span>
       </p>
       <p>
-        Service Status:{" "}
-        <span style={{ fontWeight: "bold", color: statusColor }}>
-          {data?.service?.status?.charAt(0)?.toUpperCase() +
-            data?.service?.status?.slice(1)}
-        </span>
-      </p>
-      <p>
         Service Name:{" "}
         <span style={{ fontWeight: "bold" }}>{data?.service?.title}</span>
-      </p>
-      <p>
-        Service Price:{" "}
-        <span style={{ fontWeight: "bold" }}>${data?.service?.price}</span>
       </p>
       <p>
         Rating: <span style={{ fontWeight: "bold" }}>{data?.rating}</span>
