100dd55a5162d7b57be59e39c9b4322f96a4542c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-24 11:41:12 +0600

    show title in icon

--- src/app/(mainlayout)/layout.tsx
+++ src/app/(mainlayout)/layout.tsx
@@ -2,6 +2,8 @@
 
 import CallOut from "@/components/ui/CallOut";
 import MainNavbar from "@/components/ui/MainNavbar";
+import { GithubOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 import { Footer } from "antd/es/layout/layout";
 import React from "react";
 
@@ -14,6 +16,10 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
       <CallOut />
       <Footer style={{ textAlign: "center" }}>
         Renovator ©{year} Created by yusuf
+        <div style={{ height: "160px" }}>
+          <Button></Button>
+          <GithubOutlined title="Click to view Github Code" />
+        </div>
       </Footer>
     </div>
   );
