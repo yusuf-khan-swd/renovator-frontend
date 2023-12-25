ad0ae4c867bedbf53a15ac09108af9db7f498b56
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-25 12:44:56 +0600

    Refactor name

--- src/components/ui/ActionBar.tsx
+++ src/components/ui/ActionBar.tsx
@@ -5,19 +5,19 @@ type ActionBarProps = {
 };
 
 const ActionBar = ({ title, children, style }: ActionBarProps) => {
-  const childrenStyle = {
+  const baseChildrenStyle = {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
     margin: "10px 0px",
   };
 
-  const newChildrenStyle = { ...childrenStyle, ...style };
+  const childrenStyle = { ...baseChildrenStyle, ...style };
 
   return (
     <div>
       <h1 style={{ margin: "8px 0" }}>{title}</h1>
-      <div style={newChildrenStyle}>{children}</div>
+      <div style={childrenStyle}>{children}</div>
     </div>
   );
 };
