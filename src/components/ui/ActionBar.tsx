e0734f7c5fdb0d295585756cc99601d6bfb746b8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-25 12:40:27 +0600

    Add style property for actionbar child element

--- src/components/ui/ActionBar.tsx
+++ src/components/ui/ActionBar.tsx
@@ -1,22 +1,23 @@
 type ActionBarProps = {
   title?: string;
   children?: React.ReactElement | React.ReactNode;
+  style?: React.CSSProperties;
 };
 
-const ActionBar = ({ title, children }: ActionBarProps) => {
+const ActionBar = ({ title, children, style }: ActionBarProps) => {
+  const childrenStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: "10px 0px",
+  };
+
+  const newChildrenStyle = { ...childrenStyle, ...style };
+
   return (
     <div>
       <h1 style={{ margin: "8px 0" }}>{title}</h1>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          margin: "10px 0px",
-        }}
-      >
-        {children}
-      </div>
+      <div style={newChildrenStyle}>{children}</div>
     </div>
   );
 };
