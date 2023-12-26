42e481e66f1565d368b947d7159aede54124aed2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-26 12:18:38 +0600

    Back to CenterLoading for faq and blog

--- src/app/(mainlayout)/blog/page.tsx
+++ src/app/(mainlayout)/blog/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import FullScreenLoading from "@/components/Loading/FullScreenLoading";
+import CenterLoading from "@/components/Loading/CenterLoading";
 import Accordion from "@/components/ui/Accordion";
 import { useBlogsQuery } from "@/redux/api/content/blogApi";
 
 const BlogPage = () => {
   const { data, isLoading } = useBlogsQuery(undefined);
 
-  return isLoading ? <FullScreenLoading /> : <Accordion data={data} />;
+  return isLoading ? <CenterLoading /> : <Accordion data={data} />;
 };
 
 export default BlogPage;
--- src/app/(mainlayout)/faq/page.tsx
+++ src/app/(mainlayout)/faq/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import FullScreenLoading from "@/components/Loading/FullScreenLoading";
+import CenterLoading from "@/components/Loading/CenterLoading";
 import Accordion from "@/components/ui/Accordion";
 import { useFaqsQuery } from "@/redux/api/content/faqApi";
 
 const FaqPage = () => {
   const { data, isLoading } = useFaqsQuery(undefined);
 
-  return isLoading ? <FullScreenLoading /> : <Accordion data={data} />;
+  return isLoading ? <CenterLoading /> : <Accordion data={data} />;
 };
 
 export default FaqPage;
