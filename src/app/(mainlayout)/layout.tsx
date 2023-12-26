42d7745189b6afc34e4cebb56b43afff9cf6f5ce
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-26 12:14:26 +0600

    add layout maxWidth 1580px

--- src/app/(mainlayout)/layout.tsx
+++ src/app/(mainlayout)/layout.tsx
@@ -8,7 +8,7 @@ import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div style={{ maxWidth: "1280px", margin: "0 auto" }}>
+    <div style={{ maxWidth: "1580px", margin: "0 auto" }}>
       <MainNavbar />
       <div style={{ padding: "20px 50px", minHeight: "100vh" }}>{children}</div>
       <CallOut />
