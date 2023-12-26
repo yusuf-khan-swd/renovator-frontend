dce41a672bdb2fa74c6476994fb3ac02f472caca
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-26 12:07:25 +0600

    For center loading add minHeight of 39px and width 100%

--- src/components/Loading/CenterLoading.tsx
+++ src/components/Loading/CenterLoading.tsx
@@ -2,7 +2,11 @@ import { Row, Spin } from "antd";
 
 const CenterLoading = () => {
   return (
-    <Row align="middle" justify="center">
+    <Row
+      justify={"center"}
+      align={"middle"}
+      style={{ width: "100%", minHeight: "39px" }}
+    >
       <Spin />
     </Row>
   );
