0cc4426f8f2c697c712e8760556c9c4495dee04a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-26 13:08:13 +0600

    Show services of categories

--- src/app/(mainlayout)/category/[id]/page.tsx
+++ src/app/(mainlayout)/category/[id]/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 
+import FullScreenLoading from "@/components/Loading/FullScreenLoading";
+import Service from "@/components/ui/Service";
 import { useCategoryQuery } from "@/redux/api/categoryApi";
+import { Card } from "antd";
 
 const CategoryServicesPage = ({ params }: any) => {
   const id = params?.id;
 
   const { data, isLoading } = useCategoryQuery(id);
 
-  console.log(data);
-
   const services = data?.services;
 
-  console.log(services);
-
-  return <div>Category: {data?.title}</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <FullScreenLoading />
+      ) : (
+        <Card>
+          <h3>Category: {data?.title}</h3>
+          <Service services={services} />
+        </Card>
+      )}
+    </div>
+  );
 };
 
 export default CategoryServicesPage;
