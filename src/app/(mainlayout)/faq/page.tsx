9577a197399ce5a94e3d50b913213adf5acd4b09
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-26 12:16:56 +0600

    use FullScreenLoading to faq

--- src/app/(mainlayout)/faq/page.tsx
+++ src/app/(mainlayout)/faq/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import CenterLoading from "@/components/Loading/CenterLoading";
+import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import Accordion from "@/components/ui/Accordion";
 import { useFaqsQuery } from "@/redux/api/content/faqApi";
 
 const FaqPage = () => {
   const { data, isLoading } = useFaqsQuery(undefined);
 
-  return isLoading ? <CenterLoading /> : <Accordion data={data} />;
+  return isLoading ? <FullScreenLoading /> : <Accordion data={data} />;
 };
 
 export default FaqPage;
