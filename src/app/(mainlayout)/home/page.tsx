8ac12aa0e3957cace7b8bf068a2aa52a153f6862
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-26 12:48:36 +0600

    Show categories in home page

--- src/app/(mainlayout)/home/page.tsx
+++ src/app/(mainlayout)/home/page.tsx
@@ -4,11 +4,13 @@ import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import Banner from "@/components/ui/Banner";
 import Feedback from "@/components/ui/Feedback";
 import Service from "@/components/ui/Service";
+import { useCategoriesQuery } from "@/redux/api/categoryApi";
 import {
   useOngoingServiceQuery,
   useUpcomingServiceQuery,
 } from "@/redux/api/serviceApi";
-import { Card } from "antd";
+import { Button, Card } from "antd";
+import Link from "next/link";
 
 const HomePage = () => {
   const { data: ongoingServices, isLoading: isOngoingServicesLoading } =
@@ -17,9 +19,19 @@ const HomePage = () => {
   const { data: upcomingServices, isLoading: isUpcomingServicesLoading } =
     useUpcomingServiceQuery({ limit: 2 });
 
+  const { data: categories, isLoading: categoryIsLoading } =
+    useCategoriesQuery(undefined);
+
   return (
     <div>
       <Banner />
+
+      {categories?.map((category: any) => (
+        <Link key={category?.id} href={`/category/${category?.id}`}>
+          <Button>{category?.title}</Button>
+        </Link>
+      ))}
+
       <div style={{ display: "grid", gap: "40px" }}>
         <Card>
           <h2 style={{ margin: "15px 0" }}>Ongoing Services</h2>
