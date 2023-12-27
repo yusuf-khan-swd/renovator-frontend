ecb25eb76c4b363fd846d44685c56dc0e6921c15
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-27 13:46:32 +0600

    Add custom media query style for layout

--- src/app/(mainlayout)/layout.tsx
+++ src/app/(mainlayout)/layout.tsx
@@ -3,6 +3,7 @@
 import CallOut from "@/components/ui/CallOut";
 import FooterUI from "@/components/ui/FooterUI";
 import MainNavbar from "@/components/ui/MainNavbar";
+import "./layout.css";
 
 import React from "react";
 
@@ -10,7 +11,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div style={{ maxWidth: "1580px", margin: "0 auto" }}>
       <MainNavbar />
-      <div style={{ padding: "20px 50px", minHeight: "100vh" }}>{children}</div>
+      <div className="main-layout">{children}</div>
       <CallOut />
       <FooterUI />
     </div>
