1bff7cfaac045cb5cdd4cb7284102a2e81e58521
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-28 11:28:17 +0600

    Use select to get category id

--- src/app/(mainlayout)/category/[id]/page.tsx
+++ src/app/(mainlayout)/category/[id]/page.tsx
@@ -1,19 +1,55 @@
 "use client";
 
+import { SelectOptions } from "@/components/Forms/FormSelectField";
+import CenterLoading from "@/components/Loading/CenterLoading";
 import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import Service from "@/components/ui/Service";
-import { useCategoryQuery } from "@/redux/api/categoryApi";
-import { Card } from "antd";
+import { useCategoriesQuery, useCategoryQuery } from "@/redux/api/categoryApi";
+import { Card, Select } from "antd";
+import { useState } from "react";
 
 const CategoryServicesPage = ({ params }: any) => {
   const id = params?.id;
+  const [categoryId, setCategoryId] = useState<string>(id);
 
   const { data, isLoading } = useCategoryQuery(id);
 
   const services = data?.services;
 
+  const { data: categories, isLoading: categoryIsLoading } =
+    useCategoriesQuery(undefined);
+  const categoryOptions = categories?.map((category: any) => {
+    return {
+      label: category?.title,
+      value: category?.id,
+    };
+  });
+
+  categoryOptions?.unshift({ label: "All", value: "all" });
+
+  const { data: categoryServices, isLoading: categoryServiceIsLoading } =
+    useCategoryQuery(categoryId);
+
+  console.log(categoryServices);
+
+  const filterServices = categoryServices?.services;
+
   return (
     <div style={{ margin: "10px 0" }}>
+      <div style={{ margin: "10px 0" }}>
+        {categoryIsLoading ? (
+          <CenterLoading />
+        ) : (
+          <Select
+            onChange={setCategoryId}
+            value={categoryId}
+            size={"large"}
+            options={categoryOptions as SelectOptions[]}
+            style={{ width: "100%" }}
+            placeholder={"Select category"}
+          />
+        )}
+      </div>
       {isLoading ? (
         <FullScreenLoading />
       ) : (
