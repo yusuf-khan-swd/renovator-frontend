0e0030bad163f8f0c2612c5f0dc7a3b71cca3a70
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-29 16:07:33 +0600

    Add style to call out paragraph

--- src/components/ui/CallOut.tsx
+++ src/components/ui/CallOut.tsx
@@ -4,10 +4,8 @@ const CallOut = () => {
   return (
     <div className="call-out">
       <h1 className="call-out-heading">Call: 1-888-000-8881</h1>
-      <p style={{ fontSize: "18px", color: "white", marginBottom: "10px" }}>
-        Saturday 9:00 AM - 8:00 PM Eastern
-      </p>
-      <p style={{ fontSize: "18px", color: "white" }}>
+      <p className="call-out-paragraph">Saturday 9:00 AM - 8:00 PM Eastern</p>
+      <p className="call-out-paragraph">
         Monday - Friday 8:00 AM - 9:00 PM Eastern
       </p>
     </div>
