4a64353c87d8e16f5c56496a491f33f387f1ed46
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-29 16:02:36 +0600

    Add heading css and media query

--- src/components/ui/CallOut.tsx
+++ src/components/ui/CallOut.tsx
@@ -3,9 +3,7 @@ import "./CallOut.css";
 const CallOut = () => {
   return (
     <div className="call-out">
-      <h1 style={{ fontSize: "55px", color: "white", marginBottom: "10px" }}>
-        Call: 1-888-000-8881
-      </h1>
+      <h1 className="call-out-heading">Call: 1-888-000-8881</h1>
       <p style={{ fontSize: "18px", color: "white", marginBottom: "10px" }}>
         Saturday 9:00 AM - 8:00 PM Eastern
       </p>
