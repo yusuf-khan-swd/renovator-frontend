8e8e424026ca22eca594be35f09b1a8801858fc3
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-04 12:59:42 +0600

    Update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,45 @@
 import notFoundImage from "@/assets/not_found-image.png";
+import { Button } from "antd";
 import Image from "next/image";
+import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <Image
-        src={notFoundImage}
-        width={700}
+    <div style={{ margin: "20px 4px" }}>
+      <div
         style={{
-          objectFit: "contain",
-          width: "100%",
+          textAlign: "center",
+          display: "grid",
+          gap: "6px",
         }}
-        alt="Page Not Found"
-      />
+      >
+        <h3 style={{ fontSize: "28px", color: "red" }}>Page Not found!</h3>
+        <p>
+          <Link href="/">
+            <Button>Return to home</Button>
+          </Link>
+        </p>
+      </div>
+      <div
+        style={{
+          margin: "8px auto",
+          maxWidth: "550px",
+          maxHeight: "550px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src={notFoundImage}
+          style={{
+            objectFit: "contain",
+            width: "100%",
+            height: "100%",
+          }}
+          alt="Page Not Found"
+        />
+      </div>
     </div>
   );
 };
