956ab8c004b9775bf66762fd3d707539db20c8e5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-07 13:04:17 +0600

    Add vertical scroll of 340

--- src/components/ui/CommonTable.tsx
+++ src/components/ui/CommonTable.tsx
@@ -42,7 +42,7 @@ const CommonTable = ({
       dataSource={dataSource}
       pagination={paginationConfig}
       onChange={onTableChange}
-      scroll={{ y: 940 }}
+      scroll={{ y: 340 }}
     />
   );
 };
