1e620475f719c97faf19c363a25c59afd7959b7b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-08 14:13:56 +0600

    Show five row per page

--- src/components/ManageBooking/ManageBooking.tsx
+++ src/components/ManageBooking/ManageBooking.tsx
@@ -32,7 +32,7 @@ const ManageBooking = () => {
   const query: Record<string, any> = {};
 
   const [page, setPage] = useState<number>(1);
-  const [size, setSize] = useState<number>(10);
+  const [size, setSize] = useState<number>(5);
   const [sortBy, setSortBy] = useState<string>("");
   const [sortOrder, setSortOrder] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
