92b6b3f8a69da713caef085e6b69d62b48e38902
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-09 14:33:29 +0600

    Reverse the rating options

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,9 +1,9 @@
 export const ratingOptions = [
-  { label: "1", value: "1" },
-  { label: "2", value: "2" },
-  { label: "3", value: "3" },
-  { label: "4", value: "4" },
   { label: "5", value: "5" },
+  { label: "4", value: "4" },
+  { label: "3", value: "3" },
+  { label: "2", value: "2" },
+  { label: "1", value: "1" },
 ];
 
 export const bookingStatusOptionsForAdmin = [
