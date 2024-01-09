2d555ebb20e2df3c4ceaacb385d3aba306e444d0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-09 15:17:01 +0600

    show five row per page

--- src/components/ManageReview/ManageReview.tsx
+++ src/components/ManageReview/ManageReview.tsx
@@ -30,7 +30,7 @@ const ManageReview = () => {
   const query: Record<string, any> = {};
 
   const [page, setPage] = useState<number>(1);
-  const [size, setSize] = useState<number>(10);
+  const [size, setSize] = useState<number>(5);
   const [sortBy, setSortBy] = useState<string>("");
   const [sortOrder, setSortOrder] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
