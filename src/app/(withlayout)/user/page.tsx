5dc2f596713b9bf12b12f900ef99bfbfa26f5585
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-11 13:08:20 +0600

    Call CommonBreadCrumb component in user page without items

--- src/app/(withlayout)/user/page.tsx
+++ src/app/(withlayout)/user/page.tsx
@@ -1,22 +1,10 @@
-"use client";
-
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { getUserInfo } from "@/services/auth.service";
 
 const UserPage = () => {
-  const { role } = getUserInfo() as any;
-
   return (
     <div>
-      <CommonBreadCrumb
-        items={[
-          {
-            label: `${role}`,
-            link: `/${role}`,
-          },
-        ]}
-      />
+      <CommonBreadCrumb />
 
       <UserProfile />
     </div>
