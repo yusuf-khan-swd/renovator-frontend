dd2336c20c385889515eaa39da4ea535e8f328b5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-11 13:16:25 +0600

    Update admin page with metaData and remove role from common breadcrumb

--- src/app/(withlayout)/admin/page.tsx
+++ src/app/(withlayout)/admin/page.tsx
@@ -1,24 +1,18 @@
-"use client";
-
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { getUserInfo } from "@/services/auth.service";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 // TODO: Add manage-feedbacks from home page bottom comment form.
-
 const AdminPage = () => {
-  const { role } = getUserInfo() as any;
-
   return (
     <div>
-      <CommonBreadCrumb
-        items={[
-          {
-            label: `${role}`,
-            link: `/${role}`,
-          },
-        ]}
-      />
+      <CommonBreadCrumb />
       <UserProfile />
     </div>
   );
