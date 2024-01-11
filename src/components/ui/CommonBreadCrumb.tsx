68834574c62d911f3069bd64227d1271b2939639
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-11 13:07:35 +0600

    make items array optional and if items have length then push map array to breadCrumbItems array

--- src/components/ui/CommonBreadCrumb.tsx
+++ src/components/ui/CommonBreadCrumb.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 const CommonBreadCrumb = ({
   items,
 }: {
-  items: {
+  items?: {
     label: string;
     link: string;
   }[];
@@ -26,16 +26,21 @@ const CommonBreadCrumb = ({
     {
       title: <Link href={`/${role}`}>{role}</Link>,
     },
-    ...items.map((item) => {
-      return {
-        title: item.link ? (
-          <Link href={item.link}>{item.label}</Link>
-        ) : (
-          <span>{item.label}</span>
-        ),
-      };
-    }),
   ];
+
+  if (items && items?.length > 0) {
+    breadCrumbItems.push(
+      ...items.map((item) => {
+        return {
+          title: item.link ? (
+            <Link href={item.link}>{item.label}</Link>
+          ) : (
+            <span>{item.label}</span>
+          ),
+        };
+      })
+    );
+  }
 
   return <Breadcrumb items={breadCrumbItems}></Breadcrumb>;
 };
