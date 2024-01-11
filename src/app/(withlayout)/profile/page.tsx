0d300f49bf535078783e49c34a4e47bb7ee5e936
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-11 12:53:06 +0600

    Call breadCrumb without role and remove use client for profile page

--- src/app/(withlayout)/profile/page.tsx
+++ src/app/(withlayout)/profile/page.tsx
@@ -1,20 +1,11 @@
-"use client";
-
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { getUserInfo } from "@/services/auth.service";
 
 const ProfilePage = () => {
-  const { role }: { role: string } = getUserInfo() as any;
-
   return (
     <div>
       <CommonBreadCrumb
         items={[
-          {
-            label: `${role}`,
-            link: `/${role}`,
-          },
           {
             label: "profile",
             link: `/profile`,
