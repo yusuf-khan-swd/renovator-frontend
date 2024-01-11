20cc1ce6d065c0bf5f2d14e33762a401b1d94a8d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-11 13:13:25 +0600

    Add MetaData to user page

--- src/app/(withlayout)/user/page.tsx
+++ src/app/(withlayout)/user/page.tsx
@@ -1,6 +1,13 @@
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "User - Renovator",
+  description: "A Home Renovation Service Provider",
+};
+
 const UserPage = () => {
   return (
     <div>
