d27eaa112309008ffd4a6e557a308e768eeac3bf
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-11 13:13:09 +0600

    Add metaData for profile

--- src/app/(withlayout)/profile/page.tsx
+++ src/app/(withlayout)/profile/page.tsx
@@ -1,6 +1,13 @@
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Profile - Renovator",
+  description: "A Home Renovation Service Provider",
+};
+
 const ProfilePage = () => {
   return (
     <div>
