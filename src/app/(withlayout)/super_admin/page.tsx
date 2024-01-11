1e8e44349eb1fa8e3732efde601f87b24e0192d5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-11 13:27:06 +0600

    Remove role from breadcrumb of super admin and add meta data

--- src/app/(withlayout)/super_admin/page.tsx
+++ src/app/(withlayout)/super_admin/page.tsx
@@ -1,23 +1,17 @@
-"use client";
-
 import UserProfile from "@/components/UserProfile/UserProfile";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { getUserInfo } from "@/services/auth.service";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Super Admin - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const SuperAdminPage = () => {
-  const { role } = getUserInfo() as any;
-
   return (
     <div>
-      <CommonBreadCrumb
-        items={[
-          {
-            label: `${role}`,
-            link: `/${role}`,
-          },
-        ]}
-      />
-
+      <CommonBreadCrumb />
       <UserProfile />
     </div>
   );
