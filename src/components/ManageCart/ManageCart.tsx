3c4a421c08a180d95a49eeed32179790c36cc657
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-12 15:06:41 +0600

    Show five cart item per page

--- src/components/ManageCart/ManageCart.tsx
+++ src/components/ManageCart/ManageCart.tsx
@@ -23,7 +23,7 @@ const ManageCart = () => {
   const query: Record<string, any> = {};
 
   const [page, setPage] = useState<number>(1);
-  const [size, setSize] = useState<number>(10);
+  const [size, setSize] = useState<number>(5);
   const [sortBy, setSortBy] = useState<string>("");
   const [sortOrder, setSortOrder] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
