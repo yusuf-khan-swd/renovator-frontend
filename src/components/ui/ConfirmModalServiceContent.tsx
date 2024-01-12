c073ac9a137be62ab423e42c1b92312683e2c144
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-12 15:29:16 +0600

    Extract service information to variables

--- src/components/ui/ConfirmModalServiceContent.tsx
+++ src/components/ui/ConfirmModalServiceContent.tsx
@@ -6,34 +6,37 @@ interface IServiceProps {
 }
 
 const ConfirmModalServiceContent = ({ service }: IServiceProps) => {
+  const title = service?.title;
+  const price = service?.price;
+  const location = service?.location;
+  const status = service?.status;
+  const category = service?.category?.title;
+
   const serviceStatusColor =
-    service?.status === ENUM_SERVICE_STATUS.AVAILABLE ? "green" : "blue";
+    status === ENUM_SERVICE_STATUS.AVAILABLE ? "green" : "blue";
 
   return (
     <div style={{ display: "grid", gap: "2px" }}>
       <p>
-        Service Name:{" "}
-        <span style={{ fontWeight: "bold" }}>{service?.title}</span>
+        Service Name: <span style={{ fontWeight: "bold" }}>{title}</span>
       </p>
       <p>
-        Price: <span style={{ fontWeight: "bold" }}>${service?.price}</span>
+        Price: <span style={{ fontWeight: "bold" }}>${price}</span>
       </p>
       <p>
         Location:{" "}
         <span style={{ fontWeight: "bold" }}>
-          {service?.location.charAt(0).toUpperCase() +
-            service?.location.slice(1)}
+          {location.charAt(0).toUpperCase() + location.slice(1)}
         </span>
       </p>
       <p>
         Status:{" "}
         <span style={{ fontWeight: "bold", color: serviceStatusColor }}>
-          {service?.status.charAt(0).toUpperCase() + service?.status.slice(1)}
+          {status.charAt(0).toUpperCase() + status.slice(1)}
         </span>
       </p>
       <p>
-        Category:{" "}
-        <span style={{ fontWeight: "bold" }}>{service?.category?.title}</span>
+        Category: <span style={{ fontWeight: "bold" }}>{category}</span>
       </p>
     </div>
   );
