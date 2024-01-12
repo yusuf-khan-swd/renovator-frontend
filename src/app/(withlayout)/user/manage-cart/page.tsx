6a82bce5e7f2c0b116fcfcdb093e38da3b2f643e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-12 15:05:24 +0600

    Use metadata for manage-cart page

--- src/app/(withlayout)/user/manage-cart/page.tsx
+++ src/app/(withlayout)/user/manage-cart/page.tsx
@@ -1,4 +1,10 @@
 import ManageCart from "@/components/ManageCart/ManageCart";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Cart - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const ManageCartPage = () => {
   return (
