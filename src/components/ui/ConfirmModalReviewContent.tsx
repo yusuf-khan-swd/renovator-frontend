5e75876380b9aa7f1a5b5f0da7e43687fcb7f326
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-13 12:59:41 +0600

    Use extracted information that store in variable to modal body

--- src/components/ui/ConfirmModalReviewContent.tsx
+++ src/components/ui/ConfirmModalReviewContent.tsx
@@ -9,25 +9,24 @@ const ConfirmModalReviewContent = ({ data }: IReviewProps) => {
   const email = data?.user?.email;
   const title = data?.service?.title;
   const rating = data?.rating;
-  const reviews = data?.rating;
+  const review = data?.review;
 
   return (
     <div style={{ display: "grid", gap: "2px" }}>
       <p>
-        Name: <span style={{ fontWeight: "bold" }}>{data?.user?.name}</span>
+        Name: <span style={{ fontWeight: "bold" }}>{name}</span>
       </p>
       <p>
-        Email: <span style={{ fontWeight: "bold" }}>{data?.user?.email}</span>
+        Email: <span style={{ fontWeight: "bold" }}>{email}</span>
       </p>
       <p>
-        Service Name:{" "}
-        <span style={{ fontWeight: "bold" }}>{data?.service?.title}</span>
+        Service Name: <span style={{ fontWeight: "bold" }}>{title}</span>
       </p>
       <p>
-        Rating: <span style={{ fontWeight: "bold" }}>{data?.rating}</span>
+        Rating: <span style={{ fontWeight: "bold" }}>{rating}</span>
       </p>
       <p>
-        Review: <span style={{ fontWeight: "bold" }}>{data?.review}</span>
+        Review: <span style={{ fontWeight: "bold" }}>{review}</span>
       </p>
     </div>
   );
