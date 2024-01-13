b4a5fccab80fde2062a65b82b1022cee665ebfcd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-13 12:52:34 +0600

    Create a common type for user

--- src/types/common.ts
+++ src/types/common.ts
@@ -21,6 +21,13 @@ export type IGenericErrorMessage = {
   message: string;
 };
 
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+}
+
 export interface ICategory {
   id: string;
   title: string;
