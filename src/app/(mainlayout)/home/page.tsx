596c671d59c727e0837f17d9af1e0c9e81766496
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-14 13:11:19 +0600

    Create a component for OngoingService

--- src/app/(mainlayout)/home/page.tsx
+++ src/app/(mainlayout)/home/page.tsx
@@ -2,18 +2,13 @@
 
 import HomePageCategories from "@/components/Category/HomePageCategories";
 import FullScreenLoading from "@/components/Loading/FullScreenLoading";
+import OngoingService from "@/components/Service/OngoingService";
 import Banner from "@/components/ui/Banner";
 import Feedback from "@/components/ui/Feedback";
 import Service from "@/components/ui/Service";
-import {
-  useOngoingServiceQuery,
-  useUpcomingServiceQuery,
-} from "@/redux/api/serviceApi";
+import { useUpcomingServiceQuery } from "@/redux/api/serviceApi";
 
 const HomePage = () => {
-  const { data: ongoingServices, isLoading: isOngoingServicesLoading } =
-    useOngoingServiceQuery({ limit: 2 });
-
   const { data: upcomingServices, isLoading: isUpcomingServicesLoading } =
     useUpcomingServiceQuery({ limit: 2 });
 
@@ -23,16 +18,8 @@ const HomePage = () => {
       <HomePageCategories />
 
       <div>
-        <div style={{ margin: "40px 0" }}>
-          <h2 style={{ margin: "15px 0" }}>Ongoing Services</h2>
-          {isOngoingServicesLoading ? (
-            <FullScreenLoading />
-          ) : ongoingServices?.length > 0 ? (
-            <Service services={ongoingServices} />
-          ) : (
-            <h3>No ongoing service available</h3>
-          )}
-        </div>
+        <OngoingService />
+
         <div>
           <h2 style={{ margin: "15px 0" }}>Upcoming Services</h2>
           {isUpcomingServicesLoading ? (
---
+++ src/components/Service/OngoingService.tsx
@@ -0,0 +1,23 @@
+import FullScreenLoading from "@/components/Loading/FullScreenLoading";
+import Service from "@/components/ui/Service";
+import { useOngoingServiceQuery } from "@/redux/api/serviceApi";
+
+const OngoingService = () => {
+  const { data: ongoingServices, isLoading: isOngoingServicesLoading } =
+    useOngoingServiceQuery({ limit: 2 });
+
+  return (
+    <div style={{ margin: "40px 0" }}>
+      <h2 style={{ margin: "15px 0" }}>Ongoing Services</h2>
+      {isOngoingServicesLoading ? (
+        <FullScreenLoading />
+      ) : ongoingServices?.length > 0 ? (
+        <Service services={ongoingServices} />
+      ) : (
+        <h3>No ongoing service available</h3>
+      )}
+    </div>
+  );
+};
+
+export default OngoingService;
