b6c0138642a854f3cd0d284afa642511a30504be
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-14 13:14:02 +0600

    Create UpcomingService component

--- src/app/(mainlayout)/home/page.tsx
+++ src/app/(mainlayout)/home/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
 import HomePageCategories from "@/components/Category/HomePageCategories";
-import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import OngoingService from "@/components/Service/OngoingService";
+import UpcomingService from "@/components/Service/UpcomingService";
 import Banner from "@/components/ui/Banner";
 import Feedback from "@/components/ui/Feedback";
-import Service from "@/components/ui/Service";
-import { useUpcomingServiceQuery } from "@/redux/api/serviceApi";
 
 const HomePage = () => {
-  const { data: upcomingServices, isLoading: isUpcomingServicesLoading } =
-    useUpcomingServiceQuery({ limit: 2 });
-
   return (
     <div>
       <Banner />
@@ -19,17 +14,7 @@ const HomePage = () => {
 
       <div>
         <OngoingService />
-
-        <div>
-          <h2 style={{ margin: "15px 0" }}>Upcoming Services</h2>
-          {isUpcomingServicesLoading ? (
-            <FullScreenLoading />
-          ) : upcomingServices?.length > 0 ? (
-            <Service services={upcomingServices} />
-          ) : (
-            <h3>No upcoming service available</h3>
-          )}
-        </div>
+        <UpcomingService />
       </div>
       <Feedback />
     </div>
---
+++ src/components/Service/UpcomingService.tsx
@@ -0,0 +1,23 @@
+import FullScreenLoading from "@/components/Loading/FullScreenLoading";
+import Service from "@/components/ui/Service";
+import { useUpcomingServiceQuery } from "@/redux/api/serviceApi";
+
+const UpcomingService = () => {
+  const { data: upcomingServices, isLoading: isUpcomingServicesLoading } =
+    useUpcomingServiceQuery({ limit: 2 });
+
+  return (
+    <div>
+      <h2 style={{ margin: "15px 0" }}>Upcoming Services</h2>
+      {isUpcomingServicesLoading ? (
+        <FullScreenLoading />
+      ) : upcomingServices?.length > 0 ? (
+        <Service services={upcomingServices} />
+      ) : (
+        <h3>No upcoming service available</h3>
+      )}
+    </div>
+  );
+};
+
+export default UpcomingService;
