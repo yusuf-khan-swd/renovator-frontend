910722d913c47b48077306862ddfe4811bce7237
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-14 13:08:49 +0600

    Create home page categories

--- src/app/(mainlayout)/home/page.tsx
+++ src/app/(mainlayout)/home/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import CenterLoading from "@/components/Loading/CenterLoading";
+import HomePageCategories from "@/components/Category/HomePageCategories";
 import FullScreenLoading from "@/components/Loading/FullScreenLoading";
 import Banner from "@/components/ui/Banner";
 import Feedback from "@/components/ui/Feedback";
 import Service from "@/components/ui/Service";
-import { useCategoriesQuery } from "@/redux/api/categoryApi";
 import {
   useOngoingServiceQuery,
   useUpcomingServiceQuery,
 } from "@/redux/api/serviceApi";
-import { Button, Col, Row } from "antd";
-import Link from "next/link";
 
 const HomePage = () => {
   const { data: ongoingServices, isLoading: isOngoingServicesLoading } =
@@ -20,30 +17,10 @@ const HomePage = () => {
   const { data: upcomingServices, isLoading: isUpcomingServicesLoading } =
     useUpcomingServiceQuery({ limit: 2 });
 
-  const { data: categories, isLoading: categoryIsLoading } =
-    useCategoriesQuery(undefined);
-
   return (
     <div>
       <Banner />
-
-      <div style={{ margin: "20px 0" }}>
-        {categoryIsLoading ? (
-          <CenterLoading />
-        ) : (
-          <Row gutter={{ lg: 24 }}>
-            {categories?.map((category: any) => (
-              <Col xs={24} lg={8} key={category?.id}>
-                <Link href={`/category/${category?.id}`}>
-                  <Button style={{ margin: "4px", width: "100%" }}>
-                    {category?.title}
-                  </Button>
-                </Link>
-              </Col>
-            ))}
-          </Row>
-        )}
-      </div>
+      <HomePageCategories />
 
       <div>
         <div style={{ margin: "40px 0" }}>
---
+++ src/components/Category/HomePageCategories.tsx
@@ -0,0 +1,30 @@
+import CenterLoading from "@/components/Loading/CenterLoading";
+import { useCategoriesQuery } from "@/redux/api/categoryApi";
+import { Button, Col, Row } from "antd";
+import Link from "next/link";
+
+const HomePageCategories = () => {
+  const { data: categories, isLoading: categoryIsLoading } =
+    useCategoriesQuery(undefined);
+  return (
+    <div style={{ margin: "20px 0" }}>
+      {categoryIsLoading ? (
+        <CenterLoading />
+      ) : (
+        <Row gutter={{ lg: 24 }}>
+          {categories?.map((category: any) => (
+            <Col xs={24} lg={8} key={category?.id}>
+              <Link href={`/category/${category?.id}`}>
+                <Button style={{ margin: "4px", width: "100%" }}>
+                  {category?.title}
+                </Button>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      )}
+    </div>
+  );
+};
+
+export default HomePageCategories;
