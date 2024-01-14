54d3978e3d6bc2587216799e8638d0685915a708
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-14 13:18:11 +0600

    Add metadata to home page

--- src/app/(mainlayout)/home/page.tsx
+++ src/app/(mainlayout)/home/page.tsx
@@ -4,6 +4,13 @@ import UpcomingService from "@/components/Service/UpcomingService";
 import Banner from "@/components/ui/Banner";
 import Feedback from "@/components/ui/Feedback";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Renovator",
+  description: "A Home Renovation Service Provider",
+};
+
 const HomePage = () => {
   return (
     <div>
