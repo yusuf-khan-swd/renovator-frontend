e0bd6fe5ddec9a711e3ba815f54002c2b09c939b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-15 13:25:34 +0600

    use metadata for super admin manage user

--- src/app/(withlayout)/super_admin/manage-users/page.tsx
+++ src/app/(withlayout)/super_admin/manage-users/page.tsx
@@ -1,4 +1,10 @@
 import ManageUser from "@/components/ManageUser/ManageUser";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Users - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const ManageUserPage = () => {
   return (
