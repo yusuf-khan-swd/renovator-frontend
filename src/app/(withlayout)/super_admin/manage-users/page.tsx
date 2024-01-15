9802d38f40bb10f50d7f28c03fb7d79306b5c5bf
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-15 13:00:23 +0600

    Set super admin page size of 5

--- src/app/(withlayout)/super_admin/manage-users/page.tsx
+++ src/app/(withlayout)/super_admin/manage-users/page.tsx
@@ -16,7 +16,7 @@ const AdminPage = () => {
   const [deleteUser] = useDeleteUserMutation();
 
   const [page, setPage] = useState<number>(1);
-  const [size, setSize] = useState<number>(10);
+  const [size, setSize] = useState<number>(5);
   const [sortBy, setSortBy] = useState<string>("");
   const [sortOrder, setSortOrder] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
