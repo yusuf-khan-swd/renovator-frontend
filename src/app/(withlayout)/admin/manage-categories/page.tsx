723e59f3f4ff2d8e0e262843d0de1bc4f5b96931
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-16 11:45:16 +0600

    Add metadata to manage categories

--- src/app/(withlayout)/admin/manage-categories/page.tsx
+++ src/app/(withlayout)/admin/manage-categories/page.tsx
@@ -1,4 +1,10 @@
 import ManageCategory from "@/components/ManageCategory/ManageCategory";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Categories - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const ManageCategoryPage = () => {
   return (
