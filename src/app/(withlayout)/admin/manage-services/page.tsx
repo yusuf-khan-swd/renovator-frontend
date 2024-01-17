a8f3135209509a65ffb6617726f678ee0a3065b1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-17 11:51:18 +0600

    Use metadata for manage service page

--- src/app/(withlayout)/admin/manage-services/page.tsx
+++ src/app/(withlayout)/admin/manage-services/page.tsx
@@ -1,4 +1,10 @@
 import ManageService from "@/components/ManageService/ManageService";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Services - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const ManageServicePage = () => {
   return (
