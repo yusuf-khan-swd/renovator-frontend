4cb62385f2e26c13f7cb09f3efd2ceae35d22bb3
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-20 12:03:41 +0600

    Add metadata to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import Home from "@/components/Home/Home";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const HomePage = () => {
   return (
