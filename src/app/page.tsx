24036ae211aa098d262504e650a904294f3007f0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-20 12:14:18 +0600

    Add style for home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Home from "@/components/Home/Home";
+import CallOut from "@/components/ui/CallOut";
+import FooterUI from "@/components/ui/FooterUI";
+import MainNavbar from "@/components/ui/MainNavbar";
 import { Metadata } from "next";
+import styles from "./page.module.css";
 
 export const metadata: Metadata = {
   title: "Home - Renovator",
@@ -8,8 +12,13 @@ export const metadata: Metadata = {
 
 const HomePage = () => {
   return (
-    <div>
-      <Home />
+    <div style={{ maxWidth: "1580px", margin: "0 auto" }}>
+      <MainNavbar />
+      <div className={styles["main-layout"]}>
+        <Home />
+      </div>
+      <CallOut />
+      <FooterUI />
     </div>
   );
 };
