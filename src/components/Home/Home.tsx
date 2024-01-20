34a021e90ce99eeed38e569e7b12f22de297a19f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-20 11:49:09 +0600

    Create Home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,22 @@
+import HomePageCategories from "@/components/Category/HomePageCategories";
+import OngoingService from "@/components/Service/OngoingService";
+import UpcomingService from "@/components/Service/UpcomingService";
+import Banner from "@/components/ui/Banner";
+import Feedback from "@/components/ui/Feedback";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <HomePageCategories />
+
+      <div>
+        <OngoingService />
+        <UpcomingService />
+      </div>
+      <Feedback />
+    </div>
+  );
+};
+
+export default Home;
