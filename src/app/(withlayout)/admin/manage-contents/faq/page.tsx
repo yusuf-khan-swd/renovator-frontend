4bdf802597671d08c29c9c34ec8d812deeeb7023
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-21 13:04:48 +0600

    Add metadata for manage faq page

--- src/app/(withlayout)/admin/manage-contents/faq/page.tsx
+++ src/app/(withlayout)/admin/manage-contents/faq/page.tsx
@@ -1,4 +1,10 @@
 import ManageFaq from "@/components/ManageFaq/ManageFaq";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage FAQs - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const ManageFaqPage = () => {
   return (
