b4949e1c7129f684e27259d58ba3e436704f5eee
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-21 12:45:54 +0600

    Add metadata to manage blogs

--- src/app/(withlayout)/admin/manage-contents/blog/page.tsx
+++ src/app/(withlayout)/admin/manage-contents/blog/page.tsx
@@ -1,4 +1,10 @@
 import ManageBlog from "@/components/ManageBlog/ManageBlog";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Blogs - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const ManageBlogPage = () => {
   return (
