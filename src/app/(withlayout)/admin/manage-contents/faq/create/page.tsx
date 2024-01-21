61d7a1e2ce8f2a6729b163e1f33ecbc9b5ca6047
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-21 12:51:08 +0600

    Update faq create form page

--- src/app/(withlayout)/admin/manage-contents/faq/create/page.tsx
+++ src/app/(withlayout)/admin/manage-contents/faq/create/page.tsx
@@ -8,7 +8,7 @@ import { useCreateFaqMutation } from "@/redux/api/content/faqApi";
 import { contentSchema } from "@/schemas/content";
 import { getUserInfo } from "@/services/auth.service";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Col, Row, message } from "antd";
+import { Button, Card, Col, Row, message } from "antd";
 
 const CreateServicePage = () => {
   const [createFaq] = useCreateFaqMutation();
@@ -37,27 +37,31 @@ const CreateServicePage = () => {
           { label: endRoute, link: `/${role}/${routeName}/${endRoute}` },
         ]}
       />
-      <h1>Add new faq</h1>
-      <Form submitHandler={onSubmit} resolver={yupResolver(contentSchema)}>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormInput name="title" label="Title" required />
-          </Col>
-        </Row>
-        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-          <Col span={8} style={{ margin: "10px 0" }}>
-            <FormTextArea
-              name="description"
-              label="Description"
-              rows={5}
-              required
-            />
-          </Col>
-        </Row>
-        <Button type="primary" htmlType="submit">
-          add
-        </Button>
-      </Form>
+      <div style={{ margin: "24px 5px" }}>
+        <Card>
+          <h1>Add new faq</h1>
+          <Form submitHandler={onSubmit} resolver={yupResolver(contentSchema)}>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={10} style={{ margin: "10px 0" }}>
+                <FormInput name="title" label="Title" required />
+              </Col>
+            </Row>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={18} style={{ margin: "10px 0" }}>
+                <FormTextArea
+                  name="description"
+                  label="Description"
+                  rows={9}
+                  required
+                />
+              </Col>
+            </Row>
+            <Button type="primary" htmlType="submit">
+              add
+            </Button>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 };
