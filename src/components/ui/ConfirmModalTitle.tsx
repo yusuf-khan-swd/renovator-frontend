93712f9241d19c81bbc8cdfe3117f350daf39769
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-24 12:26:58 +0600

    create a common component for modal title

---
+++ src/components/ui/ConfirmModalTitle.tsx
@@ -0,0 +1,9 @@
+const ConfirmModalTitle = ({ data }: { data: string }) => {
+  return (
+    <p>
+      Do you want to <span style={{ color: "red" }}>Delete</span> this {data}
+    </p>
+  );
+};
+
+export default ConfirmModalTitle;
