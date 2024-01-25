8891b68c5d0f8d68a4368eb273d8cdffbd9970fa
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-25 11:33:14 +0600

    Remove all the functionality from CrateFaqPage and call only CreateFaq component

--- src/app/(withlayout)/admin/manage-contents/faq/create/page.tsx
+++ src/app/(withlayout)/admin/manage-contents/faq/create/page.tsx
@@ -1,72 +1,9 @@
-"use client";
-
-import Form from "@/components/Forms/Form";
-import FormInput from "@/components/Forms/FormInput";
-import FormTextArea from "@/components/Forms/FormTextArea";
-import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { useCreateFaqMutation } from "@/redux/api/content/faqApi";
-import { contentSchema } from "@/schemas/content";
-import { getUserInfo } from "@/services/auth.service";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Card, Col, Row, message } from "antd";
+import CreateFaq from "@/components/ManageContent/ManageFaq/CreateFaq";
 
 const CreateFaqPage = () => {
-  const [createFaq] = useCreateFaqMutation();
-
-  const onSubmit = async (data: any) => {
-    try {
-      message.loading("Creating.....");
-      const result: any = await createFaq(data);
-
-      if (result?.data) {
-        message.success("Faq added successfully");
-      } else {
-        message.error("Faq added faield");
-      }
-    } catch (err: any) {
-      console.error(err.message);
-      message.error(err.message);
-    }
-  };
-
-  const { role } = getUserInfo() as any;
-  const routeLabel = "manage-faq";
-  const routeUrl = "manage-contents/faq";
-  const endRoute = "create";
-
   return (
     <div>
-      <CommonBreadCrumb
-        items={[
-          { label: routeLabel, link: `/${role}/${routeUrl}` },
-          { label: endRoute, link: `/${role}/${routeUrl}/${endRoute}` },
-        ]}
-      />
-      <div style={{ margin: "24px 5px" }}>
-        <Card>
-          <h1>Add new faq</h1>
-          <Form submitHandler={onSubmit} resolver={yupResolver(contentSchema)}>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={10} style={{ margin: "10px 0" }}>
-                <FormInput name="title" label="Title" required />
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col span={18} style={{ margin: "10px 0" }}>
-                <FormTextArea
-                  name="description"
-                  label="Description"
-                  rows={9}
-                  required
-                />
-              </Col>
-            </Row>
-            <Button type="primary" htmlType="submit">
-              add
-            </Button>
-          </Form>
-        </Card>
-      </div>
+      <CreateFaq />
     </div>
   );
 };
