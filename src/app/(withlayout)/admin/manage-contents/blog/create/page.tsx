c0fd9477bc448fc6cb902f0dd3c0dae3f5a08ee1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-25 11:44:43 +0600

    Add meta data to create blog page

--- src/app/(withlayout)/admin/manage-contents/blog/create/page.tsx
+++ src/app/(withlayout)/admin/manage-contents/blog/create/page.tsx
@@ -1,4 +1,10 @@
 import CreateBlog from "@/components/ManageContent/ManageBlog/CreateBlog";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Blog - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const CreateBlogPage = () => {
   return (
