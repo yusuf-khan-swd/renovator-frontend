ea7f0f16198ba11f16bf058d8c44c6810f958699
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-25 11:33:59 +0600

    Add metadata

--- src/app/(withlayout)/admin/manage-contents/faq/create/page.tsx
+++ src/app/(withlayout)/admin/manage-contents/faq/create/page.tsx
@@ -1,4 +1,10 @@
 import CreateFaq from "@/components/ManageContent/ManageFaq/CreateFaq";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create FAQ - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const CreateFaqPage = () => {
   return (
