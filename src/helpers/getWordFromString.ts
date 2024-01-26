72ba95908b43d76debdbe2ad3f46fdac1cf82987
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-26 12:01:06 +0600

    create a helper function getWordFromString

---
+++ src/helpers/getWordFromString.ts
@@ -0,0 +1,17 @@
+const getWordFromString = (data: string, wordCount: number) => {
+  const uptoWordCount = wordCount;
+
+  // make array of string with empty string for word then take 60 words
+  const splitData = data.split(" ");
+  const sliceSplitData = splitData.slice(0, uptoWordCount);
+
+  // Check array of words length is less then uptoWordCount if not then add ellipsis
+  const shortData =
+    splitData.length <= uptoWordCount
+      ? splitData.join(" ")
+      : sliceSplitData.join(" ") + "...";
+
+  return shortData;
+};
+
+export default getWordFromString;
