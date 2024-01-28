9477514eaf4455a3cd1de724c2586bbf4ea2f097
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-28 11:58:17 +0600

    Add metadata to feedback view and edit page

--- src/app/(withlayout)/admin/manage-feedbacks/edit/[id]/page.tsx
+++ src/app/(withlayout)/admin/manage-feedbacks/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import EditFeedback from "@/components/ManageFeedback/EditFeedback";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update Feedbacks - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const EditFeedbackPage = () => {
   return (
--- src/app/(withlayout)/admin/manage-feedbacks/view/[id]/page.tsx
+++ src/app/(withlayout)/admin/manage-feedbacks/view/[id]/page.tsx
@@ -1,4 +1,10 @@
 import DetailsFeedback from "@/components/ManageFeedback/DetailsFeedback";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Feedbacks Details - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const FeedbackDetailsPage = () => {
   return (
