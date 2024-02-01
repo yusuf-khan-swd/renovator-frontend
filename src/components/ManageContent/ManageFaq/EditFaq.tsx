f8d1b868449ac9b6a830ebf493a0a4edf2d133d3
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-01 12:49:48 +0600

    Make responsive input fields of edit faq form

--- src/components/ManageContent/ManageFaq/EditFaq.tsx
+++ src/components/ManageContent/ManageFaq/EditFaq.tsx
@@ -63,12 +63,12 @@ const EditFaq = ({ id }: { id: string }) => {
               defaultValues={defaultValues}
             >
               <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-                <Col span={10} style={{ margin: "10px 0" }}>
+                <Col xs={24} lg={10} style={{ margin: "10px 0" }}>
                   <FormInput name="title" label="Title" required />
                 </Col>
               </Row>
               <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-                <Col span={18} style={{ margin: "10px 0" }}>
+                <Col xs={24} lg={18} style={{ margin: "10px 0" }}>
                   <FormTextArea
                     name="description"
                     label="Description"
