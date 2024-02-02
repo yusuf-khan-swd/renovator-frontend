18949f9cc67177c0ef1b56889024ac52b38fc60f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-02 12:35:22 +0600

    Use div for margin

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -32,45 +32,47 @@ const UserProfile = () => {
       {isLoading ? (
         <FullScreenLoading />
       ) : (
-        <Card style={{ margin: "20px 8px" }}>
-          <FormHeading title="User Profile" />
-          <Form submitHandler={onSubmit} defaultValues={defaultValues}>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
-                <FormInput name="name" label="Name" readOnly />
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
-                <FormInput name="email" label="Email" readOnly />
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
-                <FormInput
-                  type="password"
-                  name="password"
-                  label="Password"
-                  readOnly
-                />
-              </Col>
-            </Row>
-            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-              <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
-                <FormInput name="role" label="User Role" readOnly />
-              </Col>
-            </Row>
-          </Form>
-          <Link href={`/profile/edit/${data?.id}`}>
-            <Button
-              style={{ margin: "2px" }}
-              onClick={() => console.log(data)}
-              type="primary"
-            >
-              Edit
-            </Button>
-          </Link>
-        </Card>
+        <div style={{ margin: "24px 5px" }}>
+          <Card>
+            <FormHeading title="User Profile" />
+            <Form submitHandler={onSubmit} defaultValues={defaultValues}>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
+                  <FormInput name="name" label="Name" readOnly />
+                </Col>
+              </Row>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
+                  <FormInput name="email" label="Email" readOnly />
+                </Col>
+              </Row>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
+                  <FormInput
+                    type="password"
+                    name="password"
+                    label="Password"
+                    readOnly
+                  />
+                </Col>
+              </Row>
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col xs={24} lg={12} xl={10} style={{ margin: "10px 0" }}>
+                  <FormInput name="role" label="User Role" readOnly />
+                </Col>
+              </Row>
+            </Form>
+            <Link href={`/profile/edit/${data?.id}`}>
+              <Button
+                style={{ margin: "2px" }}
+                onClick={() => console.log(data)}
+                type="primary"
+              >
+                Edit
+              </Button>
+            </Link>
+          </Card>
+        </div>
       )}
     </>
   );
