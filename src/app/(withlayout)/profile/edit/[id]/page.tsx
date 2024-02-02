c64edee576398670bd7ce3e09146b37e89199a92
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-02 12:43:40 +0600

    Add metadata for EditUserProfilePage

--- src/app/(withlayout)/profile/edit/[id]/page.tsx
+++ src/app/(withlayout)/profile/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import EditUserProfile from "@/components/UserProfile/EditUserProfile";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update User Profile - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const EditUserProfilePage = () => {
   return (
