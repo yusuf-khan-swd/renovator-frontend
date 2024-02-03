8aef061d82103c5b3bc4dad233e76f0f1c62b824
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-03 16:20:18 +0600

    Add line height and font size for description

--- src/app/(mainlayout)/about/page.tsx
+++ src/app/(mainlayout)/about/page.tsx
@@ -18,7 +18,10 @@ const AboutUsPage = () => {
                 <h3 style={{ fontSize: "26px", marginBottom: "10px" }}>
                   {about?.title}
                 </h3>
-                <p> {about?.description}</p>
+                <p style={{ lineHeight: "28px", fontSize: "16px" }}>
+                  {" "}
+                  {about?.description}
+                </p>
               </div>
             )
           )}
