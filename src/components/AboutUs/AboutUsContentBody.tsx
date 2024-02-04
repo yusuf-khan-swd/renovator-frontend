5c247326def000b2e0d0476d27bf424f26253c27
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-04 14:28:49 +0600

    Based on new line character in description show description to separate p tag

--- src/components/AboutUs/AboutUsContentBody.tsx
+++ src/components/AboutUs/AboutUsContentBody.tsx
@@ -12,10 +12,21 @@ const AboutUsContentBody = ({ data }: IAboutUsContentBodyProps) => {
   const title = data?.title;
   const description = data?.description;
 
+  const newLineRegEx = /\r\n|\r|\n/g;
+
+  const result = description.split(newLineRegEx);
+
+  const output = result.map((value, index) => <p key={index}>{value}</p>);
+
+  const isCurrentDescriptionHaveNewLine =
+    description.includes("\n") || description.includes("\r");
+
   return (
     <div style={{ marginBottom: "20px" }}>
       <h3 style={{ fontSize: "26px", marginBottom: "10px" }}>{title}</h3>
-      <p style={{ lineHeight: "28px", fontSize: "16px" }}> {description}</p>
+      <div style={{ lineHeight: "28px", fontSize: "16px" }}>
+        {isCurrentDescriptionHaveNewLine ? output : description}
+      </div>
     </div>
   );
 };
