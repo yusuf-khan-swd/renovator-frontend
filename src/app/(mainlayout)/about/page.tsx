1a9372e3b08fd66e4792b00316c32270134f01f6
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-04 13:39:38 +0600

    Add metadata for about us page

--- src/app/(mainlayout)/about/page.tsx
+++ src/app/(mainlayout)/about/page.tsx
@@ -1,4 +1,10 @@
 import AboutUs from "@/components/AboutUs/AboutUs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const AboutUsPage = () => {
   return (
