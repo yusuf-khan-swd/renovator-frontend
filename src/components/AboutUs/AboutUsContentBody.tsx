c85e3aed9895a2cd71c64249ce409a5cd0ae66da
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-04 14:38:15 +0600

    Refactor variable name for split description

--- src/components/AboutUs/AboutUsContentBody.tsx
+++ src/components/AboutUs/AboutUsContentBody.tsx
@@ -7,16 +7,16 @@ interface IAboutUsContentBodyProps {
 }
 
 const AboutUsContentBody = ({ data }: IAboutUsContentBodyProps) => {
-  console.log(data);
-
   const title = data?.title;
   const description = data?.description;
 
   const newLineRegEx = /\r\n|\r|\n/g;
 
-  const result = description.split(newLineRegEx);
+  const splitDescription = description.split(newLineRegEx);
 
-  const output = result.map((value, index) => <p key={index}>{value}</p>);
+  const mapEveryElementToPTag = splitDescription.map((value, index) => (
+    <p key={index}>{value}</p>
+  ));
 
   const isCurrentDescriptionHaveNewLine =
     description.includes("\n") || description.includes("\r");
@@ -25,7 +25,7 @@ const AboutUsContentBody = ({ data }: IAboutUsContentBodyProps) => {
     <div style={{ marginBottom: "20px" }}>
       <h3 style={{ fontSize: "26px", marginBottom: "10px" }}>{title}</h3>
       <div style={{ lineHeight: "28px", fontSize: "16px" }}>
-        {isCurrentDescriptionHaveNewLine ? output : description}
+        {isCurrentDescriptionHaveNewLine ? mapEveryElementToPTag : description}
       </div>
     </div>
   );
