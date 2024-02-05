537fb0bd7a7f2a8a7269a886a7fedd6f241f7e1d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-05 12:41:58 +0600

    Add metadata for FAQ page

--- src/app/(mainlayout)/faq/page.tsx
+++ src/app/(mainlayout)/faq/page.tsx
@@ -1,4 +1,10 @@
 import FAQ from "@/components/FAQ/FAQ";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FAQ - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const FAQPage = () => {
   return (
