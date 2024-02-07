3871ac7f6ec3392fdf82d47a745ba900042cfca2
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-07 12:21:05 +0600

    Add edit category component

---
+++ src/components/ManageCategory/EditCategory.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import FullScreenLoading from "@/components/Loading/FullScreenLoading";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import {
+  useCategoryQuery,
+  useUpdateCategoryMutation,
+} from "@/redux/api/categoryApi";
+import { categorySchema } from "@/schemas/category";
+import { getUserInfo } from "@/services/auth.service";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Card, Col, Row, message } from "antd";
+
+const EditCategory = ({ id }: { id: string }) => {
+  const { data, isLoading } = useCategoryQuery(id);
+
+  const [updateCategory] = useUpdateCategoryMutation();
+
+  const onSubmit = async (data: any) => {
+    try {
+      message.loading("Creating.....");
+      console.log(data);
+      await updateCategory(data);
+      message.success("Category updated successfully");
+    } catch (err: any) {
+      console.error(err.message);
+      message.error(err.message);
+    }
+  };
+
+  const defaultValues = {
+    id: data?.id,
+    title: data?.title || "",
+  };
+
+  const { role } = getUserInfo() as any;
+  const routeName = "manage-categories";
+  const endRoute = "edit";
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          { label: routeName, link: `/${role}/${routeName}` },
+          { label: endRoute, link: `/${role}/${routeName}/${endRoute}` },
+        ]}
+      />
+      {isLoading ? (
+        <FullScreenLoading />
+      ) : (
+        <div style={{ margin: "24px 5px" }}>
+          <Card>
+            <h1>Update category</h1>
+            <Form
+              submitHandler={onSubmit}
+              resolver={yupResolver(categorySchema)}
+              defaultValues={defaultValues}
+            >
+              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+                <Col xs={24} lg={10} style={{ margin: "10px 0" }}>
+                  <FormInput name="title" label="Title" required />
+                </Col>
+              </Row>
+              <Button type="primary" htmlType="submit">
+                Update
+              </Button>
+            </Form>
+          </Card>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EditCategory;
