1fb709508cd329356f589acf7f7309ff0c8dccec
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-07 12:15:40 +0600

    Add metadata for Create Categories

--- src/app/(withlayout)/admin/manage-categories/create/page.tsx
+++ src/app/(withlayout)/admin/manage-categories/create/page.tsx
@@ -1,4 +1,10 @@
 import CreateCategory from "@/components/ManageCategory/CreateCategory";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Categories - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const CreateCategoryPage = () => {
   return (
