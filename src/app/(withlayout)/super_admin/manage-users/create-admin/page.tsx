2f7c77747f1a5f7ad0d382ce3166cca5a95d4579
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-09 12:28:40 +0600

    Use CreateAdmin component to show CreateAdminPage information

--- src/app/(withlayout)/super_admin/manage-users/create-admin/page.tsx
+++ src/app/(withlayout)/super_admin/manage-users/create-admin/page.tsx
@@ -1,32 +1,9 @@
-"use client";
-
-import AdminForm from "@/components/AdminForm/AdminForm";
-import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { getUserInfo } from "@/services/auth.service";
+import CreateAdmin from "@/components/ManageUser/CreateAdmin";
 
 const CreateAdminPage = () => {
-  const { role } = getUserInfo() as any;
-  const routeName = "manage-users";
-  const endName = "create-admin";
-
   return (
     <div>
-      <CommonBreadCrumb
-        items={[
-          {
-            label: routeName,
-            link: `/${role}/${routeName}`,
-          },
-          {
-            label: endName,
-            link: `/${role}/${routeName}/${endName}`,
-          },
-        ]}
-      />
-
-      <div>
-        <AdminForm />
-      </div>
+      <CreateAdmin />
     </div>
   );
 };
