e2c44ed695ccf522187056dd995aa4adae187278
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-09 12:29:34 +0600

    Add metadata to CreateAdminPage component

--- src/app/(withlayout)/super_admin/manage-users/create-admin/page.tsx
+++ src/app/(withlayout)/super_admin/manage-users/create-admin/page.tsx
@@ -1,4 +1,10 @@
 import CreateAdmin from "@/components/ManageUser/CreateAdmin";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Admin - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const CreateAdminPage = () => {
   return (
