41b4cc0b4ff96ea913c4212cc296b73d0afd6bd9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-09 12:21:26 +0600

    Create a CreateUser component

---
+++ src/components/ManageUser/CreateUser.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import UserForm from "@/components/UserForm/UserForm";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import { getUserInfo } from "@/services/auth.service";
+
+const CreateUser = () => {
+  const { role } = getUserInfo() as any;
+  const routeName = "manage-users";
+  const endName = "create-user";
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          {
+            label: routeName,
+            link: `/${role}/${routeName}`,
+          },
+          {
+            label: endName,
+            link: `/${role}/${routeName}/${endName}`,
+          },
+        ]}
+      />
+      <div>
+        <UserForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreateUser;
