86883658ec37ea8a76b968aa22e1608bd204950e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-09 12:28:11 +0600

    Create a CreateAdmin component

---
+++ src/components/ManageUser/CreateAdmin.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import AdminForm from "@/components/AdminForm/AdminForm";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import { getUserInfo } from "@/services/auth.service";
+
+const CreateAdmin = () => {
+  const { role } = getUserInfo() as any;
+  const routeName = "manage-users";
+  const endName = "create-admin";
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          {
+            label: routeName,
+            link: `/${role}/${routeName}`,
+          },
+          {
+            label: endName,
+            link: `/${role}/${routeName}/${endName}`,
+          },
+        ]}
+      />
+
+      <div>
+        <AdminForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreateAdmin;
