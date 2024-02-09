abcfafceb0a9a38a2428ceeba260db4925336324
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-09 12:26:39 +0600

    Add metadata for CreateUser of superAdmin

--- src/app/(withlayout)/super_admin/manage-users/create-user/page.tsx
+++ src/app/(withlayout)/super_admin/manage-users/create-user/page.tsx
@@ -1,4 +1,10 @@
 import CreateUser from "@/components/ManageUser/CreateUser";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create User - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const CreateUserPage = () => {
   return (
