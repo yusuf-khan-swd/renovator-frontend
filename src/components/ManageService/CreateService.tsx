3a3aa143cf547ed094e4a93fd5ec4d579a14e5fc
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-12 12:11:52 +0600

    Separate create service form and functionality to CreateService component and call the component to CreateServicePage

---
+++ src/components/ManageService/CreateService.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import CategoryField from "@/components/Forms/CategoryField";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import FormSelectField, {
+  SelectOptions,
+} from "@/components/Forms/FormSelectField";
+import FormTextArea from "@/components/Forms/FormTextArea";
+import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
+import FormHeading from "@/components/ui/FormHeading";
+import { serviceCreateStatusOptions } from "@/constants/global";
+import { ENUM_SERVICE_STATUS } from "@/constants/serviceStatus";
+import { useCreateServiceMutation } from "@/redux/api/serviceApi";
+import { serviceSchema } from "@/schemas/service";
+import { getUserInfo } from "@/services/auth.service";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Card, Col, Row, message } from "antd";
+
+const CreateService = () => {
+  const [createService] = useCreateServiceMutation();
+
+  const onSubmit = async (data: any) => {
+    try {
+      message.loading("Creating.....");
+      const result: any = await createService(data);
+
+      if (result?.data) {
+        message.success("Service created successfully!");
+      } else {
+        message.error("Service create failed!");
+      }
+    } catch (err: any) {
+      console.error(err);
+      message.error(err.message);
+    }
+  };
+
+  const defaultValues = {
+    status: ENUM_SERVICE_STATUS.AVAILABLE,
+  };
+
+  const { role } = getUserInfo() as any;
+  const routeName = "manage-services";
+  const endRoute = "create";
+
+  return (
+    <div>
+      <CommonBreadCrumb
+        items={[
+          { label: routeName, link: `/${role}/${routeName}` },
+          { label: endRoute, link: `/${role}/${routeName}/${endRoute}` },
+        ]}
+      />
+      <div style={{ margin: "24px 5px" }}>
+        <Card>
+          <FormHeading title="Add a New Service" />
+          <Form
+            submitHandler={onSubmit}
+            resolver={yupResolver(serviceSchema)}
+            defaultValues={defaultValues}
+          >
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormInput name="title" label="Title" required />
+              </Col>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormInput name="price" label="Price $" required />
+              </Col>
+            </Row>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <CategoryField name="categoryId" label="Category" required />
+              </Col>
+            </Row>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormInput name="location" label="Location" required />
+              </Col>
+              <Col span={8} style={{ margin: "10px 0" }}>
+                <FormSelectField
+                  name="status"
+                  label="Status"
+                  options={serviceCreateStatusOptions as SelectOptions[]}
+                  required
+                />
+              </Col>
+            </Row>
+            <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+              <Col span={16} style={{ margin: "10px 0" }}>
+                <FormTextArea
+                  name="description"
+                  label="Description"
+                  rows={5}
+                  required
+                />
+              </Col>
+            </Row>
+            <Button type="primary" htmlType="submit">
+              add
+            </Button>
+          </Form>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default CreateService;
