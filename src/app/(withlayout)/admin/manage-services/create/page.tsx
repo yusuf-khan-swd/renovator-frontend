9e8c436ed23069eab0eef15ebbd81e074a25e3ce
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-12 12:12:29 +0600

    Add metadata to CreateServicePage

--- src/app/(withlayout)/admin/manage-services/create/page.tsx
+++ src/app/(withlayout)/admin/manage-services/create/page.tsx
@@ -1,4 +1,10 @@
 import CreateService from "@/components/ManageService/CreateService";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Services - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const CreateServicePage = () => {
   return (
