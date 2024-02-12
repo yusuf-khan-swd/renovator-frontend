246290da716140b05a8d003c00fea544b820a4d8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-12 12:17:11 +0600

    Add metadata to service details page

--- src/app/(withlayout)/admin/manage-services/details/[id]/page.tsx
+++ src/app/(withlayout)/admin/manage-services/details/[id]/page.tsx
@@ -1,4 +1,10 @@
 import DetailsService from "@/components/ManageService/DetailsService";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Services Details - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const DetailsServicePage = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
