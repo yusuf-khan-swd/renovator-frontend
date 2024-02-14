27e1f9f25e792eaef59db5edec1e7ee4f0641cdd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-14 12:38:42 +0600

    Add metadata to EditBookingPage

--- src/app/(withlayout)/admin/manage-bookings/edit/[id]/page.tsx
+++ src/app/(withlayout)/admin/manage-bookings/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import EditBooking from "@/components/ManageBooking/EditBooking";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Booking - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const EditBookingPage = () => {
   return (
