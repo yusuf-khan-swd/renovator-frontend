f4b4f8376d54baba6071529614cae9ac87732815
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-17 12:04:08 +0600

    Add metadata to EditReviewPage server component

--- src/app/(withlayout)/admin/manage-reviews/edit/[id]/page.tsx
+++ src/app/(withlayout)/admin/manage-reviews/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import EditReview from "@/components/ManageReview/EditReview";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Review - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const EditReviewPage = () => {
   return (
