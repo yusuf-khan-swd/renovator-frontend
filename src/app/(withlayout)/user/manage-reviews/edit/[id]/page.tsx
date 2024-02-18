76660d44706e09da748946c029d891dca1cfc1f4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-18 12:50:16 +0600

    use EditReview component for user EditReviewPage

--- src/app/(withlayout)/user/manage-reviews/edit/[id]/page.tsx
+++ src/app/(withlayout)/user/manage-reviews/edit/[id]/page.tsx
@@ -1,115 +1,17 @@
-"use client";
+import EditReview from "@/components/ManageReview/EditReview";
+import { Metadata } from "next";
 
-import Form from "@/components/Forms/Form";
-import FormSelectField, {
-  SelectOptions,
-} from "@/components/Forms/FormSelectField";
-import FormTextArea from "@/components/Forms/FormTextArea";
-import FullScreenLoading from "@/components/Loading/FullScreenLoading";
-import ReviewAndRatingHeading from "@/components/ReviewAndRating/ReviewAndRatingHeading";
-import ServiceDetailsCard from "@/components/Service/ServiceDetailsCard";
-import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { ratingOptions } from "@/constants/global";
-import { useReviewQuery, useUpdateReviewMutation } from "@/redux/api/reviewApi";
-import { reviewAndRatingSchema } from "@/schemas/reviewAndRating";
-import { getUserInfo } from "@/services/auth.service";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Card, Col, Row, message } from "antd";
-import Link from "next/link";
+export const metadata: Metadata = {
+  title: "Edit Review - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
-const EditReviewPage = ({ params }: any) => {
+const EditReviewPage = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
-  const { data, isLoading } = useReviewQuery(id);
-  const service = data?.service;
-
-  const [updateReview] = useUpdateReviewMutation();
-
-  const onSubmit = async (data: any) => {
-    try {
-      message.loading("Updating.....");
-      data.rating = parseInt(data.rating);
-
-      const result: any = await updateReview(data);
-      if (result?.data) {
-        message.success("Review and Rating updated successfully");
-      } else {
-        message.error("Review and rating update failed!");
-      }
-    } catch (err: any) {
-      console.error(err.message);
-      message.error(err.message);
-    }
-  };
-
-  const defaultValues = {
-    id: data?.id || "",
-    serviceId: data?.serviceId,
-    rating: data?.rating || "",
-    review: data?.review || "",
-  };
-
-  const { role } = getUserInfo() as any;
-  const routeName = "manage-reviews";
-  const endRoute = "edit";
 
   return (
     <div>
-      <CommonBreadCrumb
-        items={[
-          { label: routeName, link: `/${role}/${routeName}` },
-          { label: endRoute, link: `/${role}/${routeName}/${endRoute}` },
-        ]}
-      />
-      {isLoading ? (
-        <FullScreenLoading />
-      ) : (
-        <div style={{ padding: "24px 5px" }}>
-          <div style={{ marginBottom: "10px" }}>
-            <ServiceDetailsCard service={service} />
-          </div>
-
-          <Card>
-            <ReviewAndRatingHeading heading="Update Your Review" />
-            <Form
-              submitHandler={onSubmit}
-              resolver={yupResolver(reviewAndRatingSchema)}
-              defaultValues={defaultValues}
-            >
-              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-                <Col span={8} style={{ margin: "10px 0" }}>
-                  <FormSelectField
-                    name="rating"
-                    label="Rating"
-                    options={ratingOptions as SelectOptions[]}
-                    required
-                  />
-                </Col>
-              </Row>
-
-              <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
-                <Col span={16} style={{ margin: "10px 0" }}>
-                  <FormTextArea
-                    name="review"
-                    label="Review Description"
-                    rows={5}
-                    required
-                  />
-                </Col>
-              </Row>
-              <div>
-                <Button type="primary" htmlType="submit">
-                  Update Review
-                </Button>
-                <Link href={`/${role}/manage-reviews/view/${data?.id}`}>
-                  <Button type="link" style={{ margin: "0 5px" }}>
-                    View Review
-                  </Button>
-                </Link>
-              </div>
-            </Form>
-          </Card>
-        </div>
-      )}
+      <EditReview id={id} />
     </div>
   );
 };
