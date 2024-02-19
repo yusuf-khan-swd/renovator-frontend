06274ed683df4fd621033f52eec907a19cdae42f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-19 12:54:43 +0600

    Add metadata to EditUserPage

--- src/app/(withlayout)/admin/manage-users/edit/[id]/page.tsx
+++ src/app/(withlayout)/admin/manage-users/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import EditUser from "@/components/ManageUser/EditUser";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit User - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const EditUserPage = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
