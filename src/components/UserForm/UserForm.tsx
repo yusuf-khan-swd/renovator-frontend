d25294d3d3d100afbc5158686ef739b32878994d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-20 11:56:41 +0600

    Use AntDesign Card component for Wrapper for login image and create user form

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -4,7 +4,7 @@ import FormInput from "@/components/Forms/FormInput";
 import { useUserSignupMutation } from "@/redux/api/authApi";
 import { userSchema } from "@/schemas/user";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Col, Row, message } from "antd";
+import { Button, Card, Col, Row, message } from "antd";
 import Image from "next/image";
 import { SubmitHandler } from "react-hook-form";
 import loginImage from "../../assets/login-image.png";
@@ -44,68 +44,70 @@ const UserForm = () => {
         alignContent: "center",
       }}
     >
-      <Row justify="center" align="middle">
-        <Col className="login-image" lg={10}>
-          <Image src={loginImage} width={400} alt="login image" />
-        </Col>
-        <Col sm={22} md={14} lg={8}>
-          <h1
-            style={{
-              margin: "15px 0px",
-            }}
-          >
-            Create an user account
-          </h1>
-          <div>
-            <Form
-              submitHandler={onSubmit}
-              resolver={yupResolver(userSchema)}
-              defaultValues={defaultValues}
+      <Card>
+        <Row justify="center" align="middle">
+          <Col className="login-image" lg={10}>
+            <Image src={loginImage} width={400} alt="login image" />
+          </Col>
+          <Col sm={22} md={14} lg={8}>
+            <h1
+              style={{
+                margin: "15px 0px",
+              }}
             >
-              <div>
-                <FormInput
-                  name="name"
-                  type="text"
-                  size="large"
-                  label="Name"
-                  required
-                />
-              </div>
-              <div
-                style={{
-                  margin: "15px 0px",
-                }}
+              Create an user account
+            </h1>
+            <div>
+              <Form
+                submitHandler={onSubmit}
+                resolver={yupResolver(userSchema)}
+                defaultValues={defaultValues}
               >
-                <FormInput
-                  name="email"
-                  type="text"
-                  size="large"
-                  label="Email"
-                  required
-                />
-              </div>
-              <div>
-                <FormInput
-                  name="password"
-                  type="password"
-                  size="large"
-                  label="Password"
-                  required
-                />
-              </div>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{
-                  margin: "10px 0px",
-                }}
-              >
-                Create User
-              </Button>
-            </Form>
-          </div>
-        </Col>
-      </Row>
+                <div>
+                  <FormInput
+                    name="name"
+                    type="text"
+                    size="large"
+                    label="Name"
+                    required
+                  />
+                </div>
+                <div
+                  style={{
+                    margin: "15px 0px",
+                  }}
+                >
+                  <FormInput
+                    name="email"
+                    type="text"
+                    size="large"
+                    label="Email"
+                    required
+                  />
+                </div>
+                <div>
+                  <FormInput
+                    name="password"
+                    type="password"
+                    size="large"
+                    label="Password"
+                    required
+                  />
+                </div>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{
+                    margin: "10px 0px",
+                  }}
+                >
+                  Create User
+                </Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 };
