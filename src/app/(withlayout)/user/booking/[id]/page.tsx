6a7a551152d913bbf747fa4f77270f056aa606f7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-22 13:19:48 +0600

    Add metadata for Booking

--- src/app/(withlayout)/user/booking/[id]/page.tsx
+++ src/app/(withlayout)/user/booking/[id]/page.tsx
@@ -1,4 +1,10 @@
 import Booking from "@/components/Booking/Booking";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Booking - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const BookingPage = () => {
   return (
