8a64382db80695df44c8d1cf4cc747e828c62166
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-24 12:00:09 +0600

    Use endRoute with id in DetailsFeedback component Breadcrumb

--- src/components/ManageFeedback/DetailsFeedback.tsx
+++ src/components/ManageFeedback/DetailsFeedback.tsx
@@ -51,11 +51,15 @@ const DetailsFeedback = ({ id }: { id: string }) => {
 
   const { role } = getUserInfo() as any;
   const routeName = "manage-feedbacks";
+  const endRoute = "details";
 
   return (
     <div>
       <CommonBreadCrumb
-        items={[{ label: routeName, link: `/${role}/${routeName}` }]}
+        items={[
+          { label: routeName, link: `/${role}/${routeName}` },
+          { label: endRoute, link: `/${role}/${routeName}/${endRoute}/${id}` },
+        ]}
       />
       {isLoading ? (
         <FullScreenLoading />
