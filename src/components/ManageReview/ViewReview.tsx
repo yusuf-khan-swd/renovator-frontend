0cc2acbe24c7fc0e9dfd18343ed4d804aad5133f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-24 11:57:44 +0600

    Use endRoute with id in ViewReview component

--- src/components/ManageReview/ViewReview.tsx
+++ src/components/ManageReview/ViewReview.tsx
@@ -48,11 +48,15 @@ const ViewReview = ({ id }: { id: string }) => {
 
   const { role } = getUserInfo() as any;
   const routeName = "manage-reviews";
+  const endRoute = "view";
 
   return (
     <div>
       <CommonBreadCrumb
-        items={[{ label: routeName, link: `/${role}/${routeName}` }]}
+        items={[
+          { label: routeName, link: `/${role}/${routeName}` },
+          { label: endRoute, link: `/${role}/${routeName}/${endRoute}/${id}` },
+        ]}
       />
       {isLoading ? (
         <FullScreenLoading />
