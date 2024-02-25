938e73e4e680a641c1ae71e3b57acca570fbfea4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-25 12:04:32 +0600

    Add metadata to service route

--- src/app/(mainlayout)/service/page.tsx
+++ src/app/(mainlayout)/service/page.tsx
@@ -1,4 +1,10 @@
 import ServicePage from "@/components/Service/ServicePage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Service - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const Service = () => {
   return (
