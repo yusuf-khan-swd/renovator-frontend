f949cf7b094e759ec8d0501dc7a302aebc1651cd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-26 13:36:55 +0600

    create a Service details page component

--- src/app/(mainlayout)/service/[id]/page.tsx
+++ src/app/(mainlayout)/service/[id]/page.tsx
@@ -1,51 +1,11 @@
-"use client";
+import ServiceDetailsPage from "@/components/Service/ServiceDetailsPage";
 
-import ReviewAndRatingForm from "@/components/Forms/ReviewAndRatingForm";
-import FullScreenLoading from "@/components/Loading/FullScreenLoading";
-import ServiceCard from "@/components/Service/ServiceCard";
-import Reviews from "@/components/ui/Reviews";
-import { useServiceQuery } from "@/redux/api/serviceApi";
-import { getUserInfo } from "@/services/auth.service";
-import { Button } from "antd";
-import Link from "next/link";
-
-const ServiceDetailsPage = ({ params }: any) => {
-  const { role } = getUserInfo() as any;
-
-  const id = params?.id;
-  const { data: service, isLoading: isServiceLoading } = useServiceQuery(id);
-
+const ServiceDetails = () => {
   return (
     <div>
-      <h1 style={{ padding: "15px 0" }}>Service Details Page</h1>
-      {isServiceLoading ? (
-        <FullScreenLoading />
-      ) : (
-        <ServiceCard service={service} detailsPage={true} />
-      )}
-
-      <div>
-        <div style={{ margin: "40px 0", minHeight: "80px" }}>
-          {role ? (
-            <ReviewAndRatingForm serviceId={id} />
-          ) : (
-            <div>
-              Please{" "}
-              <Link href="/login">
-                <Button type="link" style={{ padding: "4px" }}>
-                  Login
-                </Button>
-              </Link>{" "}
-              to give review.
-            </div>
-          )}
-        </div>
-        <div style={{ minHeight: "120px" }}>
-          <Reviews serviceId={id} />
-        </div>
-      </div>
+      <ServiceDetailsPage />
     </div>
   );
 };
 
-export default ServiceDetailsPage;
+export default ServiceDetails;
