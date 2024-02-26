d331cff4fcbb51ff4fafc40767979ef80a08c2ce
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-02-26 13:37:25 +0600

    add metadata to service details page

--- src/app/(mainlayout)/service/[id]/page.tsx
+++ src/app/(mainlayout)/service/[id]/page.tsx
@@ -1,4 +1,10 @@
 import ServiceDetailsPage from "@/components/Service/ServiceDetailsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Service Details - Renovator",
+  description: "A Home Renovation Service Provider",
+};
 
 const ServiceDetails = () => {
   return (
