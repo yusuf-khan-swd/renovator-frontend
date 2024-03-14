d89030a989e5b54247d01d6837060f762585cee4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-03-14 11:25:04 +0600

    Update metadata of ManageNormalUserPage

--- src/app/(withlayout)/super_admin/manage-normal-users/page.tsx
+++ src/app/(withlayout)/super_admin/manage-normal-users/page.tsx
@@ -2,7 +2,7 @@ import ManageNormalUser from "@/components/ManageUser/ManageNormalUser";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Manage Admins - Renovator",
+  title: "Manage Normal Users - Renovator",
   description: "A Home Renovation Service Provider",
 };
 
