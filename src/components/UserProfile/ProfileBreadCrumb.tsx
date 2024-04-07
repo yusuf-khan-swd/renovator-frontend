fc7d0a47baddead79755baac88580d8bfcb8480b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-04-07 12:19:07 +0600

    Create a ProfileBreadCrumb component to fix the CommonBreadCrumb conflict with Profile

---
+++ src/components/UserProfile/ProfileBreadCrumb.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { getUserInfo } from "@/services/auth.service";
+import { HomeOutlined } from "@ant-design/icons";
+import { Breadcrumb } from "antd";
+import Link from "next/link";
+
+const ProfileBreadCrumb = () => {
+  const { role }: { role: string } = getUserInfo() as any;
+
+  const breadCrumbItems = [
+    {
+      title: (
+        <Link href="/">
+          <HomeOutlined />
+        </Link>
+      ),
+    },
+    {
+      title: <Link href={`/${role}`}>{role}</Link>,
+    },
+  ];
+
+  return <Breadcrumb items={breadCrumbItems} />;
+};
+
+export default ProfileBreadCrumb;
