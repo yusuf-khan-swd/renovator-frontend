53f1f9cd9277ee263e41f941c7661e0b833a142f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-04-10 12:15:16 +0600

    Use directly variable for link instead of template literal for routeName in admin EditBooking

--- src/components/ManageBooking/EditBooking.tsx
+++ src/components/ManageBooking/EditBooking.tsx
@@ -63,7 +63,7 @@ const EditBooking = ({ id }: { id: string }) => {
     <div>
       <CommonBreadCrumb
         items={[
-          { label: routeName, link: `${routeName}` },
+          { label: routeName, link: routeName },
           { label: endRoute, link: `${routeName}/${endRoute}/${id}` },
         ]}
       />
