67510df6af97d9872de11038adafdbf473a435ee
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-04-20 11:23:13 +0600

    Remove unused role variable and getUserInfo function in CreateUser

--- src/components/ManageUser/CreateUser.tsx
+++ src/components/ManageUser/CreateUser.tsx
@@ -2,10 +2,8 @@
 
 import UserForm from "@/components/UserForm/UserForm";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
-import { getUserInfo } from "@/services/auth.service";
 
 const CreateUser = ({ pageRoute }: { pageRoute?: string }) => {
-  const { role } = getUserInfo() as any;
   const routeName = pageRoute || "manage-users";
   const endName = "create-user";
 
