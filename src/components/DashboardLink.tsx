41562d44a6960c9044910ca927b9894a7a2e1c33
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-04-23 11:51:30 +0600

    Basic setup done for DashboardLink with children and pageRoute props

--- src/components/DashboardLink.tsx
+++ src/components/DashboardLink.tsx
@@ -1,5 +1,22 @@
-const DashboardLink = () => {
-  return <div></div>;
+"use client";
+
+import { getUserInfo } from "@/services/auth.service";
+import Link from "next/link";
+
+const DashboardLink = ({
+  pageRoute,
+  children,
+}: {
+  pageRoute?: string;
+  children: React.ReactNode;
+}) => {
+  const { role } = getUserInfo() as any;
+
+  return (
+    <div>
+      <Link href={`/${role}/${pageRoute}`}>{children}</Link>
+    </div>
+  );
 };
 
 export default DashboardLink;
