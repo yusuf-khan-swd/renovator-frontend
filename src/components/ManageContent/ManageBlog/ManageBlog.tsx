7c9ed4723fa04cb4e86de01fbd659152b23c16da
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-04-27 12:29:09 +0600

    Remove Edit Blog link from ManageBlog

--- src/components/ManageContent/ManageBlog/ManageBlog.tsx
+++ src/components/ManageContent/ManageBlog/ManageBlog.tsx
@@ -114,15 +114,6 @@ const ManageBlog = () => {
                 <EditOutlined />
               </Button>
             </DashboardLink>
-            <Link href={`/${role}/${routeUrl}/edit/${data?.id}`}>
-              <Button
-                style={{ margin: "2px" }}
-                onClick={() => console.log(data)}
-                type="primary"
-              >
-                <EditOutlined />
-              </Button>
-            </Link>
             <ConfirmModal
               id={data?.id}
               handler={deleteHandler}
