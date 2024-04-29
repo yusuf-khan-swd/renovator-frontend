33f1834f6225529a1ae686c03a649fa5dcfe08d4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-04-29 12:46:38 +0600

    Use the DashboardLink component for Edit Feedback link In the ViewFeedback component

--- src/components/ManageFeedback/ViewFeedback.tsx
+++ src/components/ManageFeedback/ViewFeedback.tsx
@@ -16,6 +16,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Card, Col, Row, message } from "antd";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import DashboardLink from "../DashboardLink";
 import FormHeading from "../ui/FormHeading";
 
 const ViewFeedback = ({ id }: { id: string }) => {
@@ -100,6 +101,9 @@ const ViewFeedback = ({ id }: { id: string }) => {
               </Row>
               <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
                 <Col xs={24} xl={8} style={{ margin: "10px 0" }}>
+                  <DashboardLink pageRoute={`${routeName}/edit/${id}`}>
+                    <Button type="primary">Edit Feedback</Button>
+                  </DashboardLink>
                   <Link href={`/${role}/${routeName}/edit/${id}`}>
                     <Button type="primary">Edit Feedback</Button>
                   </Link>
