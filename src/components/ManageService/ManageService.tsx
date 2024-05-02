275997fba06fdd57df282187d35ecf519a16e680
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-02 12:57:24 +0600

    Remove Edit Service Next link From ManageService component

--- src/components/ManageService/ManageService.tsx
+++ src/components/ManageService/ManageService.tsx
@@ -152,15 +152,6 @@ const ManageService = () => {
                 <EditOutlined />
               </Button>
             </DashboardLink>
-            <Link href={`/${role}/${routeName}/edit/${data?.id}`}>
-              <Button
-                style={{ margin: "2px" }}
-                onClick={() => console.log(data)}
-                type="primary"
-              >
-                <EditOutlined />
-              </Button>
-            </Link>
             <ConfirmModal
               id={data?.id}
               handler={deleteHandler}
