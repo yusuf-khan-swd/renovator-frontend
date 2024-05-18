f0a58a6517ae6c13354be249d5cf0db0ed787248
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-05-18 12:37:38 +0600

    modified style of min price

--- src/components/Service/ServicePage.tsx
+++ src/components/Service/ServicePage.tsx
@@ -213,7 +213,7 @@ const ServicePage = () => {
         {/* Maximum and Minimum price Input field   */}
         <div>
           <Row>
-            <Col xs={24} md={8} style={{ margin: "0px 10px 10px 0" }}>
+            <Col xs={24} md={8} style={{ margin: "0 10px 10px 0" }}>
               <label>Min Price</label>
               <Input
                 type="number"
