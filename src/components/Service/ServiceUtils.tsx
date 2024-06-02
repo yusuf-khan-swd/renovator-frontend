ca642a0ff017f42e07852ca5f04ed9e078ccb821
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-06-02 11:25:08 +0600

    create a function name handleSetSorting for handling and add value to state

--- src/components/Service/ServiceUtils.tsx
+++ src/components/Service/ServiceUtils.tsx
@@ -1,3 +1,5 @@
+import { SetStateAction } from "react";
+
 const MIN_MAX_PRICE = "min_max";
 const MAX_MIN_PRICE = "max_min";
 const SERVICE_NAME_ASC = "service_name_asc";
@@ -19,4 +21,50 @@ export const ServiceSortBySelectOptions = () => {
     { label: "Status  - Asc", value: SERVICE_STATUS_ASC },
     { label: "Status  - Desc", value: SERVICE_STATUS_DESC },
   ];
+};
+
+export const handleSetSorting = (
+  value: string,
+  setSortBy: (value: SetStateAction<string>) => void,
+  setSortOrder: (value: SetStateAction<string>) => void
+) => {
+  const ASC = "asc";
+  const DESC = "desc";
+
+  const PRICE = "price";
+  const TITLE = "title";
+  const LOCATION = "location";
+  const STATUS = "status";
+
+  if (!value) {
+    setSortBy("");
+    setSortOrder("");
+  } else if (value === MIN_MAX_PRICE) {
+    setSortBy(PRICE);
+    setSortOrder(ASC);
+  } else if (value === MAX_MIN_PRICE) {
+    setSortBy(PRICE);
+    setSortOrder(DESC);
+  } else if (value === SERVICE_NAME_ASC) {
+    setSortBy(TITLE);
+    setSortOrder(ASC);
+  } else if (value === SERVICE_NAME_DESC) {
+    setSortBy(TITLE);
+    setSortOrder(DESC);
+  } else if (value === SERVICE_LOCATION_ASC) {
+    setSortBy(LOCATION);
+    setSortOrder(ASC);
+  } else if (value === SERVICE_LOCATION_DESC) {
+    setSortBy(LOCATION);
+    setSortOrder(DESC);
+  } else if (value === SERVICE_STATUS_ASC) {
+    setSortBy(STATUS);
+    setSortOrder(ASC);
+  } else if (value === SERVICE_STATUS_DESC) {
+    setSortBy(STATUS);
+    setSortOrder(DESC);
+  } else {
+    setSortBy("");
+    setSortOrder("");
+  }
 };
