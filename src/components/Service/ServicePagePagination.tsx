835a3f61a1324183ccc6c318da8649d1fa566adf
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-11 10:42:38 +0600

    add some dummy text

--- src/components/Service/ServicePagePagination.tsx
+++ src/components/Service/ServicePagePagination.tsx
@@ -1,12 +1,13 @@
 import { Pagination } from "antd";
 
 const ServicePagePagination = () => {
-  const meta = { total: 10 };
+  const meta = { total: 20 };
   const size = 5;
   const onPaginationChange = () => {};
 
   return (
     <div style={{ textAlign: "center", margin: "10px 0" }}>
+      ServicePagePagination
       <Pagination
         total={meta?.total}
         defaultPageSize={size}
