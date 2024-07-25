eeac59cb4fd6c39dc2586a33bc1cc85b52cf38e1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-07-25 11:04:37 +0600

    add no faq found in faq page

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -7,7 +7,13 @@ import { useFaqsQuery } from "@/redux/api/content/faqApi";
 const FAQ = () => {
   const { data, isLoading } = useFaqsQuery(undefined);
 
-  return isLoading ? <CenterLoading /> : <Accordion data={data} />;
+  return isLoading ? (
+    <CenterLoading />
+  ) : !data || data?.length <= 0 ? (
+    <h3 style={{ margin: "2px", textAlign: "center" }}>No FAQs available</h3>
+  ) : (
+    <Accordion data={data} />
+  );
 };
 
 export default FAQ;
