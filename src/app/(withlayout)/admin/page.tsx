9eb9a49d76c85934a8ecd05926e88e96b97acef9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-03 10:32:40 +0600

    Refactor Admin page and component names

--- src/app/(withlayout)/admin/page.tsx
+++ src/app/(withlayout)/admin/page.tsx
@@ -1,4 +1,4 @@
-import UserProfile from "@/components/UserProfile/UserProfile";
+import UserProfilePage from "@/components/UserProfile/UserProfilePage";
 import CommonBreadCrumb from "@/components/ui/CommonBreadCrumb";
 
 import { Metadata } from "next";
@@ -8,13 +8,13 @@ export const metadata: Metadata = {
   description: "A Home Renovation Service Provider",
 };
 
-const AdminPage = () => {
+const Admin = () => {
   return (
     <div>
       <CommonBreadCrumb />
-      <UserProfile />
+      <UserProfilePage />
     </div>
   );
 };
 
-export default AdminPage;
+export default Admin;
