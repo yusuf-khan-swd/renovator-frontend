b28aa4d006b9d118b68fcafd13fe70a2d05dc8dd
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-04 11:38:04 +0600

    Refactor EditUser page in super_admin manage-admins

--- src/app/(withlayout)/super_admin/manage-admins/edit/[id]/page.tsx
+++ src/app/(withlayout)/super_admin/manage-admins/edit/[id]/page.tsx
@@ -1,4 +1,4 @@
-import EditUser from "@/components/ManageUser/EditUser";
+import EditUserPage from "@/components/ManageUser/EditUserPage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,14 +6,14 @@ export const metadata: Metadata = {
   description: "A Home Renovation Service Provider",
 };
 
-const EditUserPage = ({ params }: { params: { id: string } }) => {
+const EditUser = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
 
   return (
     <div>
-      <EditUser id={id} pageRoute="manage-admins" />
+      <EditUserPage id={id} pageRoute="manage-admins" />
     </div>
   );
 };
 
-export default EditUserPage;
+export default EditUser;
