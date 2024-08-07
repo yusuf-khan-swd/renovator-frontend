731a34f62e742ea3ffaea7a9aa10240c3a958550
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-07 10:37:20 +0600

    Refactor CreateUser page in admin manage-users

--- src/app/(withlayout)/admin/manage-users/create-user/page.tsx
+++ src/app/(withlayout)/admin/manage-users/create-user/page.tsx
@@ -1,4 +1,4 @@
-import CreateUser from "@/components/ManageUser/CreateUser";
+import CreateUserPage from "@/components/ManageUser/CreateUserPage";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -6,12 +6,12 @@ export const metadata: Metadata = {
   description: "A Home Renovation Service Provider",
 };
 
-const CreateUserPage = () => {
+const CreateUser = () => {
   return (
     <div>
-      <CreateUser />
+      <CreateUserPage />
     </div>
   );
 };
 
-export default CreateUserPage;
+export default CreateUser;
